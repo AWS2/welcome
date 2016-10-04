0ab6eaad337d1e8af4a4111925bb629c6e743917
Adrián Arbós <adrianarbosnavas@gmail.com>
2016-10-04 16:48:39 +0200

    Update index.php
    elimino el codigo de upload-images que GUILLEM GARRIDO habia subido

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 
 <h1>AWS2 Matrix World</h1>
 
-
-<form action="upload-image.php" method="POST" enctype="multipart/form-data">
-	<label for="imagen">Imagen:</label>
-	<input type="file" name="imagen" id="imagen" />
-	<input type="submit" name="subir" value="Subir"/>
-</form>
-
-
 <ul>
 
 <?php
