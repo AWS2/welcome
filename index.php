c9bc45f3674010a8820503c922b5422667e1a015
Marc Sola <mmarcsola@gmail.com>
2016-10-04 17:00:59 +0200

    Modificado float

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="prueba.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	
 
 <h1>AWS2 Matrix World</h1>
 
@@ -9,23 +18,35 @@
 </form>
 
 
+
+
 <ul>
 
 <?php
+
 	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+
 	foreach( $imgs as $img ) {
-		
+		echo "<div class='float'>";
 		if( substr($img,-3)=="jpg" ) {
+
 			$name = substr($img,0,-4);
+
 			echo "<li>\n";
 			echo "<a href='profile/$name.html'>\n";
 			echo "<img src='img/$img' width='130'><br/>";
 			echo $name."</a>";
 			echo "<br /><br />\n";
 			echo "</li>\n\n";
+			
 		}
+		echo "</div>";
 	}
 	
 ?>
 
+
 </ul>
+
+</body>
+</html>
