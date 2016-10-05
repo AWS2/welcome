6ec1ebef317f872f4fe6a4624815e472fd32ce9b
jaamito <gm.2012.ian.lopez@gmail.com>
2016-10-05 17:49:28 +0200

    canvi de carpetoides

--- index.php
+++ index.php
@@ -1,31 +1,12 @@
 
-<h1>AWS2 Matrix World</h1>
-
-
-<form action="upload-image.php" method="POST" enctype="multipart/form-data">
-	<label for="imagen">Imagen:</label>
-	<input type="file" name="imagen" id="imagen" />
-	<input type="submit" name="subir" value="Subir"/>
-</form>
-
-
-<ul>
-
-<?php
-	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-	foreach( $imgs as $img ) {
-		
-		if( substr($img,-3)=="jpg" ) {
-			$name = substr($img,0,-4);
-			echo "<li>\n";
-			echo "<a href='profile/$name.html'>\n";
-			echo "<img src='img/$img' width='130'><br/>";
-			echo $name."</a>";
-			echo "<br /><br />\n";
-			echo "</li>\n\n";
-		}
-	}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<a href="/var/www/html/welcome/img/Feos/">Por aqui vamos a feos</a>
 	
-?>
-
-</ul>
+</body>
+</html>
