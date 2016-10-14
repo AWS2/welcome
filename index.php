c20a3cb12655606ac2f8bbaf53156cd666c7909a
jaamito <gm.2012.ian.lopez@gmail.com>
2016-10-14 17:22:51 +0200

    cambio una cosita

--- index.php
+++ index.php
@@ -6,8 +6,35 @@
 	<title>Document</title>
 </head>
 <body>
-	
-<a href="img/Feos">Por aqui vamos a la carpeta feos</a>
-	
+<h1>Prueba</h1>
+<FORM action='index.php' method='post'>
+<SELECT name="carpeta">
+
+<?php
+		$dir = opendir("img");
+		$arxiu = readdir($dir);
+		
+			while( $arxiu!==false )
+			{
+				if($arxiu!='..' && $arxiu!= '.'){
+				echo '<OPTION value="'.$arxiu.'">'.$arxiu.'</option>';
+			}
+				$arxiu = readdir($dir);
+			}
+		closedir($dir);
+?>
+</SELECT>
+<BR />
+<INPUT type="submit" value="abrir carpeta" />
+<?php
+	if(isset($_POST["carpeta"])){
+			$estilo = "img/".$_POST["carpeta"];
+			$images = glob($estilo."*.jpg");
+				foreach($images as $image) {
+					echo '<img src="'.$image.'" /><br />';
+				}
+
+			}
+?>
 </body>
 </html>
