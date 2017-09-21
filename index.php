31bf05e28bf31af67314e68b637bb3ec0b732782
danigracia <32102969+danigracia@users.noreply.github.com>
2017-09-21 17:14:04 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -7,13 +7,15 @@
 	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 	foreach( $imgs as $img ) {
 		
-		if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+		if( substr($img,(-3 || -4))==("jpg" || "png" || "jpeg")) {
 			$name = substr($img,0,-4);
 			echo "<a href='profile/$name.html'>";
 			echo "<img src='img/$img' width='130'><br/>";
 			echo $name."</a>";
 			echo "<br /><br />";
 		}
+
+
 	}
 	
 ?>
