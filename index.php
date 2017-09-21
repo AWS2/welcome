967a46dfc5ddc7438c1f1a053de8303bd6d6bb72
root <mcolominasrojas@iesesteveterradas.cat>
2017-09-21 17:56:24 +0200

    forma basica html

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="estilos.css"/>
 
-<h1>El fabulosoo mundo de AWS2</h1>
+	</head>
+    <body>
+        <h1>El fabuloso mundo de AWS2</h1>
 
-<ul>
+	<ul>
 
-<?php
-	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-	foreach( $imgs as $img ) {
-		
-		if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-			$name = substr($img,0,-4);
-			echo "<a href='profile/$name.html'>";
-			echo "<img src='img/$img' width='130'><br/>";
-			echo $name."</a>";
-			echo "<br /><br />";
-		}
-
-
-	}
+		<?php
+			$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+			foreach( $imgs as $img ) {
 	
-?>
+				if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+					$name = substr($img,0,-4);
+					echo "<a href='profile/$name.html'>";
+					echo "<img src='img/$img' width='130'><br/>";
+					echo $name."</a>";
+					echo "<br /><br />";
+				}
 
-</ul>
+
+			}
+
+		?>
+	</ul>
+
+    </body>
+</html>
