8a9f22841d767f6e5e18f3b1c39ca5b5c1f04a5a
GODSKADI <agarcia-fraileperez@iesesteveterradas.cat>
2017-09-21 17:51:50 +0200

    prueba

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
-<h1><s>El fabuloso mundo de AWS2</s></h1>
+<h1>El fabuloso mundo de AWS2</h1>
 
 <ul>
 
@@ -7,7 +7,7 @@
 	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 	foreach( $imgs as $img ) {
 		
-		if( substr($img,(-3 || -4))==("jpg" || "png" || "jpeg")) {
+		if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 			$name = substr($img,0,-4);
 			echo "<a href='profile/$name.html'>";
 			echo "<img src='img/$img' width='130'><br/>";
