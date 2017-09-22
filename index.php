4dcc7e54983f765f7cbf3e6ef9da9c6f23ff3bce
root <mcolominasrojas@iesesteveterradas.cat>
2017-09-22 17:05:40 +0200

    css + html

--- index.php
+++ index.php
@@ -8,25 +8,25 @@
     <body>
         <h1>El fabuloso mundo de AWS2</h1>
 
-	<ul>
+        <div id="contenedorPrincipal">
+			<?php
+				$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+				foreach( $imgs as $img ) {
+					
+					if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+						
+						$name = substr($img,0,-4);
+						echo "<a href='profile/$name.html'>";
+						echo "<div class='contenido'>";
+						echo "<img src='img/$img' width='130'><br/>";
+						echo $name;
+						echo "</div></a>";
+					}
 
-		<?php
-			$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-			foreach( $imgs as $img ) {
-	
-				if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-					$name = substr($img,0,-4);
-					echo "<a href='profile/$name.html'>";
-					echo "<img src='img/$img' width='130'><br/>";
-					echo $name."</a>";
-					echo "<br /><br />";
+
 				}
 
-
-			}
-
-		?>
-	</ul>
-
+			?>
+		</div>
     </body>
 </html>
