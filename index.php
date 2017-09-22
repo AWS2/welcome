9f0409cdaa435bd83c8a81e0c613330caf1d59cd
Adrian Cardenas <SonHak@github.com>
2017-09-22 16:49:14 +0200

    avance

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <body>
-	<h1></h1>
+	<h1> Participamos en un concurso de diseño (sale mal) </h1>
 	<link rel="stylesheet" type="text/css" href="index.css">
 	<ul>
 
@@ -7,7 +7,7 @@
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 		foreach( $imgs as $img ) {
 			
-			if( substr($img,(-3 || -4))==("jpg" || "png" || "jpeg")) {
+			if( substr($img,-3)=="jpg" or substr($img,3)=="png" or substr($img,4)=="jpeg"){
 				$name = substr($img,0,-4);
 				echo "<a href='profile/$name.html'>";
 				echo "<img src='img/$img' width='130'><br/>";
