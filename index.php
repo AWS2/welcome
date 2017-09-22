16f7f4a998b3c854fed90b74d43ab7278457b1e3
GenisTorresDonaire <gtorresdonaire@iesesteveterradas.cat>
2017-09-22 17:00:03 +0200

    Mod PHP

--- index.php
+++ index.php
@@ -2,24 +2,19 @@
 <link rel="stylesheet" type="text/css" href="css_jg.css">
 </head>
 <h1>Galeria de Jordi i Genis</h1>
-
-<ul>
-
-<?php
-	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-	foreach( $imgs as $img ) {
-		
-		if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-			$name = substr($img,0,-4);
-			echo "<a href='profile/$name.html'>";
-			echo "<img src='img/$img' width='130'><br/>";
-			echo $name."</a>";
-			echo "<br /><br />";
-		}
-
-
-	}
-	
-?>
-
-</ul>
+	<ul>
+		<?php
+			$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+			foreach( $imgs as $img ) {
+				if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+					$name = substr($img,0,-4);
+					echo "<div>";
+					echo "<a class='enlaces' href='profile/$name.html'>";
+					echo "<img class='imagenes' src='img/$img' width='130'><br/>";
+					echo $name."</a>";
+					echo "<br /><br />";
+					echo "</div>";
+				}
+			}
+		?>
+	</ul>
