d51fe48cc49599db6d94a93c3cb28d4a0b9981a8
danigracia <gracia.danii@gmail.com>
2017-09-22 16:37:13 +0200

    css

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
+<style>
+    <?php include 'styles.css'; ?>
+</style>
+<div class="header">
+    <h1>El fabuloso mundo de AWS2</h1>
+</div>
 
-<h1>El fabuloso mundo de AWS2</h1>
 
 <ul>
 
@@ -7,17 +12,24 @@
 	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 	foreach( $imgs as $img ) {
 		
-		if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+		if( substr($img,-3)=="jpg" or substr($img,-3)=="png") {
 			$name = substr($img,0,-4);
-			echo "<a href='profile/$name.html'>";
-			echo "<img src='img/$img' width='130'><br/>";
+            echo "<img src='img/$img' width='130'><br/>";
+            echo "<a href='profile/$name.html'>";
 			echo $name."</a>";
 			echo "<br /><br />";
 		}
 
+		elseif (substr($img,-4)=="jpeg"){
+            $name = substr($img,0,-5);
+            echo "<img src='img/$img' width='130'><br/>";
+            echo "<a href='profile/$name.html'>";
+            echo $name."</a>";
+            echo "<br /><br />";
+        }
+
 
 	}
-	
 ?>
 
 </ul>
