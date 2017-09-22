1a9eb53c2c1e979fee18ddf7b9ab13038a6e8f2e
KrinpyOfficial <mortegarodriguez@iesesteveterradas.cat>
2017-09-22 18:57:38 +0200

    Cambio en Index.php

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 
-<h1>El fabuloso mundo de AWS2</h1>
+<head>
+	<link href="CSSGit.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<h1>El fabuloso mundo de AWS2</h1>
 
-<ul>
+	<?php 
 
-<?php
-	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-	foreach( $imgs as $img ) {
-		
-		if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-			$name = substr($img,0,-4);
-			echo "<a href='profile/$name.html'>";
-			echo "<img src='img/$img' width='130'><br/>";
-			echo $name."</a>";
-			echo "<br /><br />";
+		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+		foreach( $imgs as $img ) {	
+			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+				$name = substr($img,0,-4);
+				echo "<a href='profile/$name.html'>";
+				echo "<img src='img/$img' width='130'>";
+				echo $name."</a>";
+				echo "<div></div>";
+				;
+			}
 		}
+	?>
 
-
-	}
-	
-?>
-
-</ul>
+</body>
