3533e6d3164fe7d2a02586952c0e53f24a74f221
Adrian Cardenas <SonHak@github.com>
2017-09-22 16:30:15 +0200

    fondo

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
+<body>
+	<h1></h1>
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<ul>
 
-<h1>El fabuloso mundo de AWS2</h1>
-<link rel="stylesheet" type="text/css" href="index.css">
-<ul>
+	<?php
+		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+		foreach( $imgs as $img ) {
+			
+			if( substr($img,(-3 || -4))==("jpg" || "png" || "jpeg")) {
+				$name = substr($img,0,-4);
+				echo "<a href='profile/$name.html'>";
+				echo "<img src='img/$img' width='130'><br/>";
+				echo $name."</a>";
+				echo "<br /><br />";
+			}
 
-<?php
-	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-	foreach( $imgs as $img ) {
-		
-		if( substr($img,(-3 || -4))==("jpg" || "png" || "jpeg")) {
-			$name = substr($img,0,-4);
-			echo "<a href='profile/$name.html'>";
-			echo "<img src='img/$img' width='130'><br/>";
-			echo $name."</a>";
-			echo "<br /><br />";
+
 		}
+		
+	?>
 
-
-	}
-	
-?>
-
-</ul>
+	</ul>
+</body>
