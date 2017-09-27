bb547c5cd353d59b3de6d129beaf0f57fef1b40c
danigracia <gracia.danii@gmail.com>
2017-09-27 16:36:21 +0200

    update

--- index.php
+++ index.php
@@ -7,29 +7,26 @@
 
 
 <ul>
+<table>
+    <?php
+        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+        foreach( $imgs as $img ) {
 
-<?php
-	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-	foreach( $imgs as $img ) {
-		
-		if( substr($img,-3)=="jpg" or substr($img,-3)=="png") {
-			$name = substr($img,0,-4);
-            echo "<img src='img/$img' width='130'><br/>";
-            echo "<a href='profile/$name.html'>";
-			echo $name."</a>";
-			echo "<br /><br />";
-		}
+                if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+                    $name = substr($img,0,-4);?>
+                    <tr>
+                        <td>
+                            <?php echo "<a href='profile/$name.html'><img src='img/$img' width='130'></a><br/>";
+                                  echo "<h3>";
+                                  echo $name."</h3>";?>
+                        </td>
+                    </tr>
+               <?php }?>
+            <?php
 
-		elseif (substr($img,-4)=="jpeg"){
-            $name = substr($img,0,-5);
-            echo "<img src='img/$img' width='130'><br/>";
-            echo "<a href='profile/$name.html'>";
-            echo $name."</a>";
-            echo "<br /><br />";
+
         }
-
-
-	}
-?>
+    ?>
+</table>
 
 </ul>
