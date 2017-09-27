349744afffc91d2d7beaa77254bae6e76f1521d3
danigracia <gracia.danii@gmail.com>
2017-09-27 17:36:56 +0200

    spam colors

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <div class="header">
     <h1>El fabuloso mundo de AWS2</h1>
 </div>
+<img src="img/rana.gif" width="80" style="position: absolute; top: 0; left: 0; margin: 0 0 0 20px;" />
+<img src="img/rana.gif" width="80" style="position: absolute; top: 0; left: 0; margin: 0 0 0 20px;" />
 
 
 <ul>
