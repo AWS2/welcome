70eab77caa1cc63c273d70487acbca6fe9828658
danigracia <gracia.danii@gmail.com>
2017-09-29 16:57:30 +0200

    update

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <div class="header">
     <h1>El fabuloso mundo de AWS2</h1>
 </div>
-<img src="img/rana.gif" width="80" style="position: absolute; top: 0; left: 0; margin: 0 0 0 20px;" />
-<img src="img/rana.gif" width="80" style="position: absolute; top: 0; left: 0; margin: 0 0 0 20px;" />
+<img src="img/rana.gif" width="80" style="position: absolute; top: 2px; left: 3%;" />
+<img src="img/rana.gif" width="80" style="position: absolute; top: 2px; left: 93%;" />
 
 
 <ul>
