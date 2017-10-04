055b9f0fea6adc4450d04e257ff6f23268a655bf
marc98sjd <mguerraexposito@iesesteveterradas.cat>
2017-10-04 18:59:42 +0200

    he afegit php i he retocat alguns errors

--- index.php
+++ index.php
@@ -7,9 +7,18 @@ body {
 	padding-left: 2.5em;
 }
 
-h1 {
+h1{
 	margin-top: 1em;
 	font-size: 50px;
+	font-family:Helvetica;
+	text-shadow: 0px 0px 6px rgba(255,255,255,0.7);
+	color: white;
+	text-align: center;
+}
+
+p{
+	margin-top: 1em;
+	font-size: 20px;
 	font-family:Helvetica;
 	text-shadow: 0px 0px 6px rgba(255,255,255,0.7);
 	color: white;
@@ -50,7 +59,7 @@ button {
 	box-shadow: 0px 5px 0px #111319;
 }
 
-p {
+div p {
 	font-style: none;
 	text-decoration: none;
 	color: black;
@@ -63,26 +72,47 @@ p {
 </style>
 </head>
 <body>
-<h1>La p**a m****a de vida de AWS2</h1>
+	<h1>La p**a m****a de vida de AWS2</h1>
+	<?php
+			//codi per mostrar un missatge de benvinguda utilitzant cookies
+			if(isset($_COOKIE['AboutVisit'])){ 
+		 		$last = $_COOKIE['AboutVisit']; } 
+		 		$year = 31536000 + time() ; 
+		 		//aixo afegeix un any al temps actual per l'expiració de cookies 
+		 		setcookie(AboutVisit, time (), $year) ; 
+		 	if (isset ($last)){
+		 		$change = time () - $last; 
+		 		if($change > 86400){
+					echo "<p>Benvingut de nou! <br> L'última visita va ser el ". date("m/d/y",$last); 
+					echo "</p>";
+					// Aquí imprimeixo un missatge en cas que fagi mes d'un dia que han visitat la pàgina
+				}
+		 		else{ 
+		 			echo "<p>Hola! Que has oblidat res?!"; 
+		 			echo "</p>";
+					//aquí mostro un missatge si la persona visita la pàgina mes d'un cop el mateix dia
+				} 
+			}
+			else{ 
+		 		echo "<p>Benvingut a la nostra clase!"; 
+		 		echo "</p>";
+		 		//missatge en cas que visiti per primer cop la pàgina 
+		 	} 
+	?>
 
-<ul>
-
-<?php
-	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-	foreach( $imgs as $img ) {
-		
-		if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-			$name = substr($img,0,-4);
-			echo "<div><img src='img/$img' width='130'><br />";
-			echo "<p>";
-			echo $name."</p><br><br>";
-			echo "<a href='profile/$name.html'><button>Mostrar página</button></a></div>";
-		}
-
-
-	}
-	
-?>
-
-</ul>
+	<ul>
+		<?php 
+			$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+			foreach( $imgs as $img ) {
+				
+				if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+					$name = substr($img,0,-4);
+					echo "<div><img src='img/$img' width='130'><br />";
+					echo "<p>";
+					echo $name."</p><br><br>";
+					echo "<a href='profile/$name.html'><button>Mostrar página</button></a></div>";
+				}
+			}
+		?>
+	</ul>
 </body>
