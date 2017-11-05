7712f99cb1fc632cd535407f994fa95a896286f9
Rodriguez Gomez <manuel.rodriguez-gomez@hpe.com>
2017-11-05 12:57:06 +0100

    Los archivos de Manu
    Actualizar los ficheros

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
+<style>
+    <?php include 'estilo.css'; ?>
+</style>
+<div class="header">
+    <h1>Concurso de estilo de Manuel_Red Hat</h1>
+</div>
 
-<body>
-	<h1> Concurso de estilo de Manuel_Red Hat</h1>
-	<link rel="stylesheet" type="text/css" href="estilo.css">
-	<ul>
-
-	<?php 
-
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
-				;
-			}
-		}
-
-	?>
-
-</body>
+<ul>
+    <table>
+        <?php
+            $imgs = scandir("./img",SCANDIR_SORT_DESCENDING);
+            foreach( $imgs as $img ) {
+                    if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+                        $name = substr($img,0,-4);?>
+                        <tr>
+                            <td>
+                                <?php echo "<a href='profile/$name.html'><img  src='img/$img' width='130'></a><br/>";
+                                      echo "<h3>";
+                                      echo $name."</h3>";?>
+                            </td>
+                        </tr>
+                   <?php }?>
+                <?php
+            }
+        ?>
+    </table>
+</ul>
