79c6334c08d572cb8ac99b3e7336d758810e0c06
Kevin Sedano Carrillo <ksedanocarrillo@iesesteveterradas.cat>
2018-09-24 17:30:13 +0200

    primer commit golosones

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Projecte Welcome 1</title>
+</head>
 
 <body>
 	<h1>Projecte Welcome 1</h1>
+	<link rel="stylesheet" type="text/css" href="welcome.css" />
+
 	<ul>
 
 	<?php 
@@ -10,8 +16,8 @@
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
 				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
+				echo "<img src='img/$img' width='130'>\n<br>";
+				echo $name."</a>\n<br>";
 				
 
 				echo "<div></div>";
