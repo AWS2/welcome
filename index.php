0545072c0dbdd7e95bb7747ccc99ea5d53bfbf59
JoseLomero <jlomerooro@iesesteveterradas.cat>
2018-09-24 17:59:43 +0200

    actu

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-
+<link rel="stylesheet" href="style.css" type="text/css">
 <body>
 	<h1>Projecte Welcome 1</h1>
 	<ul>
@@ -10,7 +10,7 @@
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
 				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
+				echo "<img src='img/$img'>";
 				echo $name."</a>";
 				
 
