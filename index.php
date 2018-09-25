2cd37701a5bbd706e72fa8da6fec3633e9d781a6
Kevin Sedano Carrillo <ksedanocarrillo@iesesteveterradas.cat>
2018-09-25 19:56:32 +0200

    tercer commit golosones

--- index.php
+++ index.php
@@ -4,10 +4,21 @@
 </head>
 
 <body id="cuerpo" background="fondoWelcome.png">
-	<h1 align="center">Projecte Welcome 1</h1>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $('.zoom').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+});
+</script>
+
+	<h1 align="center" id="encabezado">Projecte Welcome 1</h1>
 	<link rel="stylesheet" type="text/css" href="welcome.css" />
 
-	<ul>
+	<ul id="alumnos">
 
 	<?php 
 
@@ -16,10 +27,7 @@
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
 				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>\n<br>";
-				echo $name."</a>";
-
-				echo "<div></div>";
+				echo "<img src='img/$img' width='160' height='160'  title='$name' class='zoom'";
 				;
 			}
 		}
