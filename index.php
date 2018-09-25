9b489021d011f0b9bd2f606d583277c7d989d562
nilarrus <nilarrus69@gmail.com>
2018-09-25 20:29:54 +0200

    Casi acabado

--- index.php
+++ index.php
@@ -1,27 +1,28 @@
 <head>
 	<title>Galeria AWS2</title>
+	<link rel="stylesheet" href="css/Css.css">
 </head>
 <body>
-	<div>
+	<div class="titulo">
 	<h1>Galeria de la Classe AWS2 Curso 2018-2019</h1>
 </div>
-	<ul>
-
+	<div class="imagenes">
 	<?php
  	$i =0;
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 		foreach( $imgs as $img ) {
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
-				echo "<div>";
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				echo "</div>";
+				echo "<div class='Img'>\n";
+				echo "<a href='profile/$name.html' target='_blank'>\n";
+				echo "<img src='img/$img' width='130'>\n";
+				echo "<br>".$name."</a>\n";
+				echo "</div>\n";
 			}
 		}
 
 	?>
+</div>
 <footer>
 	<p> Editado por Cristian y Nil </p>
 </footer>
