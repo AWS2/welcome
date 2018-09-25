d40f899d48be60123a44a2da8d854b231d46df34
root <erusebiru@gmail.com>
2018-09-25 20:13:01 +0200

    Subida

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
+<html>
+	<head></head>
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<body>
+		<h1>Projecte Welcome 1</h1>
+		<ul>
+		<div>
+		<?php 
+			$people = [];
 
-<body>
-	<h1>Projecte Welcome 1</h1>
-	<ul>
-
-	<?php 
-
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
+			$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+			foreach( $imgs as $img ) {	
 				
-
-				echo "<div></div>";
-				;
+				if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+					$name = substr($img,0,-4);
+					$person = array("name" => $name,"image" => $img);
+					$people[] = $person;
+				}
 			}
-		}
+			foreach($people as $value){
+				?><img src="img/<?=$value["image"]?>" height="300" width="200" alt=""><?
+			}
+		?>
+		</div>
 
-	?>
-
-</body>
+	</body>
+</html>
