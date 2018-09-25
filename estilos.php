6588323b2f0681a6d054b0189d0ce98e77354b7f
miguelarteaga <marteagavalle@gmail.com>
2018-09-25 20:10:56 +0200

    1rCommit

---
+++ estilos.php
@@ -0,0 +1,30 @@
+<?php header("Content-type: text/css"); ?>
+h1{
+	font-family:Impact,Courier New ;
+	color: red;
+}
+
+body{
+	background: black;
+}
+
+
+img {
+margin: 20px;
+border: 5px solid  #ffffff ;
+-webkit-box-shadow: 4px 4px 4px rgba(0,0,0,0.2);
+-moz-box-shadow: 4px 4px 4px rgba(0,0,0,0.2);
+box-shadow: 4px 4px 4px rgba(0,0,0,0.2);
+-webkit-transition: all 0.5s ease-out;
+-moz-transition: all 0.5s ease;
+-ms-transition: all 0.5s ease;
+}
+img:hover {
+-webkit-transform: rotate(-7deg);
+-moz-transform: rotate(-7deg);
+-ms-transform: rotate(-7deg);
+transform: rotate(-7deg);
+}
+
+	
+
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilos.php" />
+	<title></title>
+</head>
 <body>
-	<h1>Projecte Welcome 1</h1>
+	<h1 align="center">Projecte Welcome 1</h1>
 	<ul>
 
 	<?php 
@@ -9,16 +14,11 @@
 		foreach( $imgs as $img ) {	
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
-				;
+				echo "<a href='profile/$name.html'>\n";
+				echo "<img src='img/$img' width='350' height='300'>";
+				"</a>\n";
 			}
 		}
-
 	?>
-
 </body>
+</html>
