95e17117392f0bed69c6189065c9a440e017fed7
rayaalmudena <raya.almudena@gmail.com>
2018-10-01 18:51:21 +0200

    final

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 	<img class="bg" src="giphy.gif">
 	<script type="text/javascript">
 		if ( window.addEventListener ) {  
-		  var state = 0, konami = [38,38,40,40,37,39,37,39,66,65];  
+		  var state = 0, konami = [38,38,40,40,37,39,37,39,66,65, 13];  
 		  window.addEventListener("keydown", function(e) {  
 		    if ( e.keyCode == konami[state] ) state++;  
 		    else state = 0;  
-		    if ( state == 10 )  
+		    if ( state == 11 )  
 		      window.location = "konami.html";
 		}, true);  
 		}  
