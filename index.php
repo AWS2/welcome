f4723e1197700472686de4d41618f9e6817c2e51
root <jesusenmanuelsoto@gmail.com>
2018-10-01 17:42:40 +0200

    4º commit

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 <script>
 $(document).ready(function(){
     $('.zoom').hover(function() {
-        $(this).addClass('transition');
+        $(this).addClass('rotation');
     }, function() {
-        $(this).removeClass('transition');
+        $(this).removeClass('rotation');
     });
 });
 </script>
@@ -19,7 +19,7 @@ $(document).ready(function(){
 	<link rel="stylesheet" type="text/css" href="welcome.css" />
 
 	<ul id="alumnos">
-
+<div id="opaco">
 	<?php 
 
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
@@ -33,5 +33,18 @@ $(document).ready(function(){
 		}
 
 	?>
+</div>
+<?php 
 
+		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+		foreach( $imgs as $img ) {	
+			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+				$name = substr($img,0,-4);
+				echo "<a href='profile/$name.html'>";
+				echo "<img src='img/$img' width='160' height='160'  title='$name' class='zoom'";
+				;
+			}
+		}
+
+	?>
 </body>
