43dab75d7672f500b4772982df6a168fd99e3a18
root <jsotomoriano@esteveterradas.cat>
2018-10-01 17:58:48 +0200

    first commit

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
 <body>
 	<h1>Projecte Welcome 1</h1>
-	<ul>
 
 	<?php 
 
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 		foreach( $imgs as $img ) {	
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
+				echo "<div id='contenedor'>";
+					echo "<div id='contenedor-imagen'>";
+							echo "<a href='profile/$name.html'>";
+							echo "<img style='float:left' src='img/$img' width='130'>";
+					echo "</div>";
+				echo "</div>";
+				echo "</div>";
 				;
 			}
 		}
 
 	?>
-
 </body>
+
+</html>
