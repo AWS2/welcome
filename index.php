49b7427a300ec7d9184d21d95db17cd92db543a7
Dencina1996 <davidencina1996@gmail.com>
2018-10-02 19:06:18 +0200

    Commit

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
-
+<title>Casa Tarradellas</title>
+<link rel="shortcut icon" href="chorizo.jpg" />
 <body>
 	<link rel="stylesheet" type="text/css" href="index.css">
-	<h1>Projecte Welcome 1</h1>
+	<img src="Title.gif" class='Titulo'>
 	<ul>
-
+	<div class="container">
 	<?php 
-
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 		foreach( $imgs as $img ) {	
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
+				echo "<div>";
+				echo "<a href='profile/$name.html'>\n";
+				echo "<br>";
+				echo "<img src='img/$img' alt='Avatar' class='Images' title='$name'>\n";
+				echo "<h6>".$name."</h6>";
+				echo "</div>";
 
-				echo "<div></div>";
-				;
 			}
 		}
 
 	?>
-
+</div>
 </body>
