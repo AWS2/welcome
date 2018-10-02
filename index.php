6f8cda789fa7cb855a73c6f5fe30628804409e72
EricPerLuq <eperezluque@iesesteveterradas.cat>
2018-10-02 18:43:16 +0200

    Eric Commit

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
 
-<body>
+	<title></title>
+</head>
+<body background="blue">
+
 	<link rel="stylesheet" type="text/css" href="index.css">
 	<h1>Projecte Welcome 1</h1>
-	<ul>
+	<ul>	</ul>
 
 	<?php 
 
@@ -10,16 +16,21 @@
 		foreach( $imgs as $img ) {	
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
 				
+				echo " <a href='profile/$name.html'>\n ";
+				
+				echo "       ".	"<img src='img/$img' width='150\n' alt='avatar' class='image' title='$name'>";
+			
+				
+				echo " \n"	.$name."</a>\n";				
 
-				echo "<div></div>";
+				//echo "<div></div>";
 				;
 			}
 		}
 
 	?>
 
+
 </body>
+</html>
