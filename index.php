d84dcff0c1b750dec33d6194c2e5e1540dc954d0
root <malvarezruiz@iesesteveterradas.cat>
2018-10-08 16:42:22 +0200

    hola

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body class="body">
+    <h1 class="h1"> Concurs CSS Marc i Khalid</h1>
+    <div>
 
-<body>
-	<h1>Projecte Welcome 1</h1>
-	<ul>
+    <?php
+        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+        foreach( $imgs as $img ) {   
+            if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+                $name = substr($img,0,-4);
+                echo "<div class='c'>";
+                echo "<a class='a' href='profile/$name.html'>";
+                echo "<img class='img' src='img/$img'>";
+                echo $name."</a>";
+               
+                echo "</div>";
+                ;
+            }
+        }
 
-	<?php 
-
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
-				;
-			}
-		}
-
-	?>
-
+    ?>
+    </div>
 </body>
+</html>
