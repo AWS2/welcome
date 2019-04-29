6c90b5b500d13234c6d9b3fa34a50cd5215a3eaa
root <scomacorcuera@iesesteveterradas.cat>
2019-04-29 17:22:41 +0200

    firstCommit

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <head>
 	<link href="CSSGit.css" rel="stylesheet" type="text/css">
 </head>
-<body>
-	<h1>El fabuloso mundo de AWS2</h1>
+<body >
+	<h1 style='text-align: center'>El fabuloso mundo de AWS2</h1>
 
 	<?php 
 
@@ -11,10 +11,16 @@
 		foreach( $imgs as $img ) {	
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
+				echo "<div style='text-align: center'>";
 				echo "<a href='profile/$name.html'>";
 				echo "<img src='img/$img' width='130'>";
+				echo "<br>";
 				echo $name."</a>";
 				echo "<div></div>";
+				echo "</div>";
+				echo "<br>";
+				echo "<br>";
+
 				;
 			}
 		}
