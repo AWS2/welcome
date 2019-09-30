ac3211315c7dd7278e5d2c0e1233ef33d7415602
marcbryan <marcboakyeflores98@gmail.com>
2019-09-30 17:37:01 +0200

    inline-flex

--- index.php
+++ index.php
@@ -3,21 +3,23 @@
 </head>
 <body>
 	<h1>Projecte Welcome 1</h1>
-	<ul>
+	<div id="container">
+		<ul>
 
-	<?php
+		<?php
 
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<div>";
-				echo "<a class='imagenes' href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo "<p>".$name."</p></a></div>";
+			$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+			foreach( $imgs as $img ) {
+				if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+					$name = substr($img,0,-4);
+					echo "<div class='inner'>";
+					echo "<a class='imagenes' href='profile/$name.html'>";
+					echo "<img src='img/$img' width='130'>";
+					echo "<p>".$name."</p></a></div>";
+				}
 			}
-		}
 
-	?>
+		?>
+	</div>
 
 </body>
