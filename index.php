8f66e611ba62dc5e1c7cf349793b08d4a8d631a4
Ruben Hernandez <rubenhc99@gmail.com>
2019-09-30 17:29:28 +0200

    Vincular HTML CSS

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
-
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="sobrasada.css" type="text/css" rel="stylesheet">
+</head>
 <body>
-	<h1>Projecte Welcome 1</h1>
-	<ul>
+    <h1>Projecte Welcome 1</h1>
+    <ul>
+        <?php
+        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+        foreach( $imgs as $img ) {
+            if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+                $name = substr($img,0,-4);
+                echo "<a href='profile/$name.html'>";
+                echo "<img src='img/$img' width='130'>";
+                echo $name."</a>";
 
-	<?php 
-
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
-				;
-			}
-		}
-
-	?>
-
+                echo "<div></div>";
+                ;
+            }
+        }
+        ?>
 </body>
+</html>
+
