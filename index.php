37d48e1def5af47d4e4f2bab5be81bb2b4d4ecd9
computing-student <informatica.s.132@gmail.com>
2019-09-30 17:28:52 +0200

    commit3

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
+<html>
+<head>
+	<title>Welcome</title>
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+
+ <div class="header">
+  <h1>Projecte Welcome 1</h1>
+</div> 
 
 <body>
-	<h1>Projecte Welcome 1</h1>
+	
 	<ul>
 
 	<?php 
@@ -8,13 +17,16 @@
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 		foreach( $imgs as $img ) {	
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+
+				echo "<div>";
+
 				$name = substr($img,0,-4);
 				echo "<a href='profile/$name.html'>";
 				echo "<img src='img/$img' width='130'>";
 				echo $name."</a>";
 				
 
-				echo "<div></div>";
+				echo "</div>";
 				;
 			}
 		}
@@ -22,3 +34,4 @@
 	?>
 
 </body>
+</html>
