43c2614b3f7c8c444ce8b880294d321bfa4f4589
Ferran Herrero <ferran.herrerosoto@gmail.com>
2019-09-30 17:40:24 +0200

    Versión 0.0.1 CSS - ByFerran

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 
 <body>
+	<link rel="stylesheet" href="ferryrafa.css">
 	<h1>Projecte Welcome 1</h1>
-	<ul>
-
-	<?php 
-
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
-				;
+		<?php
+			$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+			foreach( $imgs as $img ) {
+				if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+					$name = substr($img,0,-4);
+					echo '<div class="polaroid">';
+						echo "<img src='img/$img' width=100%;>";
+						echo "<div class='container'>";
+							echo "<a href='profile/$name.html' target='_blank'>" . $name . "</a>";
+						echo "</div>";
+					echo "</div>";
+				}
 			}
-		}
 
-	?>
 
+
+			?>
 </body>
