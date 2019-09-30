971203539d88118a3d2a8a426aad9cef0a10c10d
rafa <rjuanferragut@gmail.com>
2019-09-30 17:58:48 +0200

    blas r

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
 				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
+				echo "<img src='img/$img' width='130'></a>";
+				echo "<p>".$name."</a>";
 				
 
 				echo "<div></div>";
