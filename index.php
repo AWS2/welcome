0f3002171e1666a991a736567b077f42728ea3d8
marcbryan <marcboakyeflores98@gmail.com>
2019-09-30 16:46:09 +0200

    CSS2

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
-
+<head>
+	<link rel="stylesheet" href="styles.css">
+</head>
 <body>
 	<h1>Projecte Welcome 1</h1>
 	<ul>
 
-	<?php 
+	<?php
 
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
+		foreach( $imgs as $img ) {
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
 				echo "<a href='profile/$name.html'>";
 				echo "<img src='img/$img' width='130'>";
 				echo $name."</a>";
-				
+
 
 				echo "<div></div>";
 				;
