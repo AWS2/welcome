6f1e4b04bd61906406b404d3691a8c4396f07f8e
Ruben Hernandez <rubenhc99@gmail.com>
2019-10-01 21:58:36 +0200

    Add id's and change title

--- index.php
+++ index.php
@@ -2,26 +2,26 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>AWS2</title>
     <link href="sobrasada.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-    <h1>Projecte Welcome 1</h1>
-    <ul>
+    <h1>Welcome AWS2</h1>
+    <div id="container">
         <?php
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
         foreach( $imgs as $img ) {
             if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-4);
                 echo "<a href='profile/$name.html'>";
+                echo "<div class='content'>";
                 echo "<img src='img/$img' width='130'>";
-                echo $name."</a>";
-
-                echo "<div></div>";
-                ;
+                echo "<span class='author'>".$name."</span>";
+                echo "</div></a>";
             }
         }
         ?>
+    </div>
 </body>
 </html>
 
