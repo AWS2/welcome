9d393be305519f897a94a86621e77bab5d56986e
Gooicoo <didaclopez93@gmail.com>
2019-10-01 17:00:41 +0200

    suegundo commi

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	 <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
 <body>
-	<h1>Projecte Welcome 1</h1>
+	<div class="cabeza">
+		<h3>Alumnos AWS!!!</h3><hr>
+	</div>
+
+
 	<ul>
+		<div class="todas">
+			<?php
+				$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+				foreach( $imgs as $img ) {
+					if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+						$name = substr($img,0,-4);
+						echo "<div class=caja>"."<a href='profile/$name.html'>";
+						echo "<img src='img/$img' width='100%' height='100%'>";
+						echo "<p>".$name."</p>"."</a>"."</div>";
 
-	<?php 
-
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
-				;
-			}
-		}
-
-	?>
+						echo "<div></div>";
+						;
+					}
+				}
+			?>
+		</div>
 
 </body>
+
+</html>
