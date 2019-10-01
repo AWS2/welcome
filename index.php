9148836d45fe82a9f4f30ccdb5750d5a7284f273
Andreu Martínez <andreu.ma.ma@hotmail.com>
2019-10-01 19:49:29 +0200

    AndreuCSS

--- index.php
+++ index.php
@@ -1,26 +1,25 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
 <body>
 	<h1>Projecte Welcome 1</h1>
 	<ul>
-
 	<?php 
-
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 		foreach( $imgs as $img ) {	
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
 				echo "<a href='profile/$name.html'>";
 				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
+				echo "$name.</a>";
 				echo "<div></div>";
-				;
 			}
 		}
-
 	?>
-<p>HOLAAAAAAAAA SOLDADO GEARS</p>
-<p>Aqui Marcus Fenix</p>
-
+	</ul>
 </body>
+</html>
+
