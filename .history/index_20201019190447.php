9ae4f1fb2006c71b4df72ffe01a69efc3e07826c
silviadlc <57746795+silviadlc@users.noreply.github.com>
2020-10-19 19:11:19 +0200

    templates folder and style file added

--- .history/index_20201019190447.php
+++ .history/index_20201019190447.php
@@ -4,11 +4,10 @@
 	<meta charset="UTF-8">
 	<title>Projecte Welcome 1</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="templates/style.css">
 </head>
 <body>
 	<h1>Projecte Welcome 1</h1>
-	<ul
+	<ul>
 
 	<?php 
 
