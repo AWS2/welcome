2be5be079af084d22874a03510b5c29b1b177ec0
jcsalinas20 <jcsalinasn20@gmail.com>
2020-10-19 18:46:28 +0200

    change index.php

--- index.php
+++ index.php
@@ -8,27 +8,25 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-	<h1>Projecte Welcome 1</h1>
-	<ul>
-
+	<div class="row">
+		<h1 class='col-sm-12 text-center w-100 p-3'>Projecte Welcome 1</h1>
+	</div>
+	<div class="row">
 	<?php 
 
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 		foreach( $imgs as $img ) {	
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
-				;
+				echo "<div class='col-lg-3 row'>";
+				echo "<div class='col-lg-8 m-auto'>";
+				echo "<a class='' href='profile/$name.html'><img src='img/$img' width='130'>$name.</a>";
+				echo "</div>";
+				echo "</div>";
 			}
 		}
 
 	?>
-
+	</div>
 </body>
-
 </html>
