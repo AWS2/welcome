df2a3ac1495db8fdf80c19e73d65c8df1b9a81c8
Alejandro Nieto Luque <aleexnl@MacBook-Pro-de-Alejandro.local>
2020-10-19 17:58:07 +0200

    project refractor

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		foreach( $imgs as $img ) {	
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
+				echo "<a href='pages/$name.html'>";
 				echo "<img src='img/$img' width='130'>";
 				echo $name."</a>";
 				
