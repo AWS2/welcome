68c0c2fd49f6153bd3bfd4d555cfe7662d06badf
Iker Cayero <iker.1997.19@gmail.com>
2020-10-19 19:04:26 +0200

    Cards

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Projecte Welcome 1</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 	<h1>Projecte Welcome 1</h1>
@@ -14,18 +15,17 @@
 		foreach( $imgs as $img ) {	
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
+				echo "
+					<div class='card' style='width: 18rem;'>
+						<img class='card-img-top' src='img/$img' alt='Card image cap'>
+						<div class='card-body'>
+							<a href='profile/$name.html' class='btn btn-primary'>$name</a>
+						</div>
+					</div>
+				"
 				;
 			}
 		}
-
 	?>
-
 </body>
 </html>
-
