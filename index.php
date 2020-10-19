2734b69a33bc2845560d03e4abaefc8dd7b6709c
jcsalinas20 <jcsalinasn20@gmail.com>
2020-10-19 18:17:24 +0200

    add image to background

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
 <body>
 	<h1>Projecte Welcome 1</h1>
 	<ul>
@@ -22,3 +29,5 @@
 	?>
 
 </body>
+
+</html>
