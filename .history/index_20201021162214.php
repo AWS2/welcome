7f84f4ae014283d898f48c9e8849ec00374b3da5
silviadlc <57746795+silviadlc@users.noreply.github.com>
2020-10-21 16:23:44 +0200

    style update

--- .history/index_20201021162214.php
+++ .history/index_20201021162214.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="templates/style.css">
 </head>
-<body style="margin: 100px 230px; background-color: #EBF3F1;">
+<body style="margin: 100px 230px; background-color: #FFFBD1;">
 	<div class="title" style="text-align:center; margin-bottom: 5%;">
 		<h1 style="font-family: oswald; text-shadow: 2px 2px 6px #8FA0FC, 2px 2px 5px #00f;">PROJECTE WELCOME 1</h1>
 	</div>	
