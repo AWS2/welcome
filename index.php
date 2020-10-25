9875b52746cd50f79e50f3dfb67c9ef61b49c219
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-10-25 19:26:51 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
+<head>
+	  <link rel="stylesheet" href="main.css">
+	  <link href="https://fonts.googleapis.com/css2?family=Anton&family=Krona+One&family=PT+Serif:wght@700&display=swap" rel="stylesheet">
+	  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+</head>
+<body>		
+	<div  class="titulo">
+		<h1>Projecte Welcome 1</h1>
+		<p>HECHO POR SERGIO GARCIA Y DAVID CASTILLA</p>
+	</div>
+	<div class="general">
+		<?php 
 
-<body>
-	<h1>Projecte Welcome 1</h1>
-	<ul>
-
-	<?php 
-
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
-				;
+			$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+			$count = 0;
+			
+			foreach( $imgs as $img ) {
+				if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+						$name = substr($img,0,-4);
+						echo '<div class="tamaño">';
+						echo "<div class='decor1'><a href='profile/$name.html'>" . $name .'</th></a></div>';
+						echo "<div class ='decor2'><img src='img/$img' width='130' height='130'></div>";
+						echo "</div>";
+				}
 			}
-		}
 
-	?>
+		?>
+	</div>
 
 </body>
