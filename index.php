51dc773306052f9f9d2874e5252bc9d6a8e21ab7
Alejandro Nieto <alex.nieto0027@gmail.com>
2020-10-25 11:57:40 +0100

    Styling

--- index.php
+++ index.php
@@ -1,24 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Alumnos</title>
+	<link rel="stylesheet" href="main.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<?php
+$imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
+$counter = 0;
+?>
 
 <body>
-	<h1>Projecte Welcome 1</h1>
-	<ul>
+	<!--<?= print_r($imgs); ?>-->
+	<table class="striped-table contain-table">
+		<thead>
+			<tr>
+				<td colspan="4">
+					<h3>ALUMNOS</h3>
+				</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<?php
+				foreach ($imgs as $img) {
+					if (substr($img, -3) == "jpg" or substr($img, -3) == "png" or substr($img, -4) == "jpeg") {
+						$name = substr($img, 0, -4);
+						echo "<td><a href='pages/$name.html'><img src='img/$img' width='100' height='100' alt='$name'></a></td>";
+						$counter++;
+						if ($counter % 4 == 0) {
+							echo "</tr><tr>";
+						}
+					}
+				}
 
-	<?php 
-
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='pages/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
-				;
-			}
-		}
-
-	?>
-
+				?>
+		</tbody>
+	</table>
 </body>
+
+</html>
