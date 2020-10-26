4d40c7b62ce8300fe76b3fced712e296a3bb1fe6
aaron <yw8ok94o@protonmail.ch>
2020-10-26 18:00:41 +0100

    Added font to title, and fixed filename bug in index.php

--- index.php
+++ index.php
@@ -5,22 +5,41 @@
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-		<h1>Project Welcome 1</h1>
+		<center><h1>Project Welcome 1</h1></center>
 
 		<div id="box-initial">
 			<?php 
+
+				$valid_ext = ["jpg", "png", "jpeg"];
 				$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 				foreach( $imgs as $img ) {	
-					if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-						$name = substr($img,0,-4);
-						echo '
-						<a href="profile/'.$name.'.html">
-							<div class="image" style="background-image: url(img/'.$img.');">
-								<div id="username" title="'.$name.'">'.$name.'</div>
-							</div>
-						</a>
-								';
+					$a = explode(".", $img);
+					
+					if(sizeof($a) >= 2){
+						$extension = $a[sizeof($a)-1];
+						
+						if(in_array($extension, $valid_ext)){
+							
+							unset($a[sizeof($a)-1]);
+							$name = "";
+							foreach($a as $b){
+								$name .= $b;
+							}
+
+							if(file_exists(str_replace("index.php", "", $_SERVER['SCRIPT_FILENAME']) . "profile/$name.html")){
+								echo '
+							<a href="profile/'.$name.'.html">
+								<div class="image" style="background-image: url(img/'.$img.');">
+									<div id="username" title="'.$name.'">'.$name.'</div>
+								</div>
+							</a>
+									';
+							}
+							
+							
+						}
 					}
+	
 				}
 			?>
 		</div>
