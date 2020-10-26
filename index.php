e6632e2b0c17422540ad052472a055521fbb7d80
bycarlos28 <carlosfernandez28correa@gmail.com>
2020-10-26 18:56:20 +0100

    editando index.php

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CarlosRohan</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+	<body>
+		<h1>Projecte Welcome 1</h1>
+		<ul>
 
-<body>
-	<h1>Projecte Welcome 1</h1>
-	<ul>
+		<?php 
 
-	<?php 
-
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
+			$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+			foreach( $imgs as $img ) {	
+				if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+					$name = substr($img,0,-4);
+					echo "<a href='profile/$name.html'>";
+					echo "<img src='img/$img' width='130'>";
+					echo $name."</a>";
 				
 
-				echo "<div></div>";
-				;
+					echo "<div></div>";
+					;
+				}
 			}
-		}
 
-	?>
-
-</body>
+		?>
+		</ul>
+	</body>
+</html>
