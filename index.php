a64491302635b6bdc3240275f1c88a1830860ee0
bycarlos28 <carlosfernandez28correa@gmail.com>
2021-04-19 17:48:18 +0200

    final commit

--- index.php
+++ index.php
@@ -5,24 +5,27 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 	<body>
-		<h1>Projecte Welcome 1</h1>
-		<div id="cuerpo">
-
-			<?php 
-
-				$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-				foreach( $imgs as $img ) {	
-					if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-						$name = substr($img,0,-4);
-						echo "<a href='profile/$name.html'>";
-						echo "<img src='img/$img' width='130'>";
-						echo $name."</a>";
-						echo "<div></div>";
-						;
+		<div id="titulo">
+			<h1>Projecte Welcome 1</h1>
+		</div>
+		<div id="body">
+			<div id="cuerpo">
+				<?php 
+					$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+					foreach( $imgs as $img ) {	
+						if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+							$name = substr($img,0,-4);
+							echo "<div class='cuerpo2'>";
+							echo "<a href='profile/$name.html'>";
+							echo "<img src='img/$img' width='130'>";
+							echo "<p>$name.</p></a>";
+							echo "<div></div>";
+							echo "</div>";
+							;
+						}
 					}
-				}
-
-			?>
+				?>
+			</div>
 		</div>
 	</body>
 </html>
