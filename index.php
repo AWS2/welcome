870b77b71d913e2f7d0a1bc047deb65fb18ce328
Carlos <carmuny54@gmail.com>
2021-10-04 18:50:37 +0200

    maquetarlo

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
-
+<head>
+	<link rel="stylesheet" href="style.css">
+</head>
 <body>
 	<h1>Projecte Welcome 1</h1>
-	<ul>
 
-	<?php 
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				echo "<div></div>";
-			}else if (substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-5);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				echo "<div></div>";
+	<div class="container">
+		<?php 
+			$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+			foreach( $imgs as $img ) {	
+				if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
+					$name = substr($img,0,-4);
+				}else if (substr($img,-4)=="jpeg") {
+					$name = substr($img,0,-5);
+				}
+				?>
+
+				<div class="marco">
+					<div class="flex">
+						<?php
+							echo "<a href='profile/$name.html'><img src='img/$img' href='profile/$name.html' width='130'> </a>";
+							echo "<a class='name' href='profile/$name.html'>$name</a>";
+							echo "<div></div>";
+						?>
+					</div>
+				</div>
+				<?php
 			}
-		}
-	?>
+		?>
+
+	</div>
 
 </body>
