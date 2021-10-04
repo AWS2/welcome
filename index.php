800f469da82aa745359b252ffba0be019d020d5f
super <edupedu101.eduardvd2001@hotmail.com>
2021-10-04 18:31:40 +0200

    añadido margenes

--- index.php
+++ index.php
@@ -9,16 +9,17 @@
 
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 		foreach( $imgs as $img ) {
-			echo "<div>";	
+				
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+				echo "<div class='imagen'>";
 				$name = substr($img,0,-4);
 				echo "<a href='profile/$name.html'>";
 				echo "<img src='img/$img' width='130'>";
 				echo "<p>".$name."</p></a>";
-			
+				echo "</div>";
 				
 			}
-			echo "</div>";
+			
 		}
 
 	?>
