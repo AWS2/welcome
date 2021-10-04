1654a33af6a6a1f1ecd9f2ff0b60e36c6df47c21
martiLlo <martillorachnaharro@gmail.com>
2021-10-04 18:28:01 +0200

    flexbox implementado

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Projecte Welcome 1</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
 <body>
 	<h1>Projecte Welcome 1</h1>
-	<ul>
+	<div class="contenedor">
 
 	<?php 
 
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
 		foreach( $imgs as $img ) {	
+			
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+				echo "<div class=subcontenedor>";
 				$name = substr($img,0,-4);
 				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
+				echo "<img src='img/$img' width='130'><br>";
 				echo $name."</a>";
-				
-
-				echo "<div></div>";
-				;
+				echo "</div>";
 			}
 		}
 
 	?>
-
+	</div>
 </body>
