ca42d72038747297cad405e877ba16a6b0f2115c
pvalls4 <valls.vilalta.4@gmail.com>
2021-10-04 18:30:29 +0200

    Fixeado la incompatibilidad con imagenes .jpeg

--- index.php
+++ index.php
@@ -4,21 +4,18 @@
 	<ul>
 
 	<?php 
-
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
-
-				echo "<div></div>";
-				;
-			}
-		}
-
-	?>
+        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+        foreach( $imgs as $img ) {    
+            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
+                $name = substr($img,0,-4);
+            }else if (substr($img,-4)=="jpeg") {
+                $name = substr($img,0,-5);
+            }
+            echo "<a href='profile/$name.html'>";
+            echo "<img src='img/$img' width='130'>";
+            echo $name."</a>";
+            echo "<div></div>";
+        }
+    ?>
 
 </body>
