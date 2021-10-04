be248c53c765661d3edec54a976cc57ced719dba
Sousa <oscar.sousa2001@gmail.com>
2021-10-04 18:06:48 +0200

    Colorines super chulis

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <head>
-	 <link rel="stylesheet" type="text/css" href="stylesheet.css">
+<link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
+	<div style="width:100%;display:flex;justify-content:center;margin-bottom:30px">
 	<h1>Projecte Welcome 1</h1>
+</div>
 	<ul>
-
 	<?php 
 
 		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+		echo "<div style='display:flex;width:100%;flex-wrap:wrap'>";
 		foreach( $imgs as $img ) {	
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
+
+				echo "<div>";
 				echo "<a href='profile/$name.html'>";
 				echo "<img src='img/$img' width='130'>";
 				echo $name."</a>";
-				
-
+				echo "</div>";
 				echo "<div></div>";
 				;
 			}
 		}
-
+		echo "</div>";
 	?>
 
 </body>
