e76fc3034c755aa456dbfce80397cb8ec3f2575d
PauTormo <73992423+PauTormo@users.noreply.github.com>
2021-10-04 19:24:26 +0200

    Actualizar css

--- index.php
+++ index.php
@@ -1,24 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link rel="stylesheet" href="hola.css" type="text/css">
+</head>
 
 <body>
-	<h1>Projecte Welcome 1</h1>
-	<ul>
-	<link rel="stylesheet" href="hola.css" type="text/css">
-	<?php 
+    <div class="header">
+        <h1>Projecte Welcome 1</h1>
+    </div>
+    <ul>
+    
+    <?php 
 
-		$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		foreach( $imgs as $img ) {	
-			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-				$name = substr($img,0,-4);
-				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
-				echo $name."</a>";
-				
+        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+        foreach( $imgs as $img ) {  
+            if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+                $name = substr($img,0,-4);
+                echo "<a href='profile/$name.html'>";
+                echo "<img src='img/$img' width='130'>";
+                echo $name."</a>";
+                
 
-				echo "<div></div>";
-				;
-			}
-		}
 
-	?>
+                ;
+            }
+        }
+
+    ?>
 
 </body>
+
+</html>
