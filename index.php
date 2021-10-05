4586f2ce85cd5e50dd313891e55b8f10c826b08f
SamuelMillan <samuel.millanperez01@gmail.com>
2021-10-05 18:17:22 +0200

    Commit foto samuel

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
                         if(substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
                                 $name = substr($img,0,-4);
                                 $nameLowerCase = strtolower($name);
-                                if ($nameLowerCase ==  "joseparedes" || $nameLowerCase == "adriangofe") {
+                                if ($nameLowerCase ==  "joseparedes" || $nameLowerCase == "adriangofe" || $nameLowerCase == "samuelmillan") {
                                         echo "<a href='profile/$name.html'>";
                                         echo "<center><img src='img/$img' width='130'></center>";
                                         echo $name."</a>";
