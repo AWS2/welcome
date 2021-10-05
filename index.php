6971cf7c9835e7a56f100ab4a287f7111ca7a8ba
super <JoseParedes1>
2021-10-05 18:06:21 +0200

    Commit index.php

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
-
 <body>
-	<h1>Projecte Welcome 1</h1>
-	<ul>
+        <h1><center>Equipo TeamSAJA</center></h1>
+        <ul>
 
-	<?php 
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {    
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-        }
-    ?>
+        <?php 
 
-</body>
+                $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+                foreach( $imgs as $img ) {
+                        if(substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+                                $name = substr($img,0,-4);
+                                $nameLowerCase = strtolower($name);
+                                if ($nameLowerCase == "adriangofe" || $nameLowerCase ==  "joseparedes") {
+                                        echo "<a href='profile/$name.html'>";
+                                        echo "<center><img src='img/$img' width='130'></center>";
+                                        echo $name."</a>";
+                                }
+                                echo "<div></div>";
+                                ;
+                        }
+                }
+
+        ?>
+
+</body> 
