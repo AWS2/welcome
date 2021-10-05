6d451c5aaf13a5193f0ccf1bebfc35ea597dcbc0
juandee99 <urbanojuandedios@gmail.com>
2021-10-05 19:14:48 +0200

    commit final juande

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 			if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
 				$name = substr($img,0,-4);
 				echo "<a href='profile/$name.html'>";
-				echo "<img src='img/$img' width='130'>";
+				echo "<img src='img/$img' width='130'class='imgRedonda'>";
 				echo $name."</a>";
 				
 
