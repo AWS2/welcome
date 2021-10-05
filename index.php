e5fdffd96db506841afa4a63f4185e56cf189309
Adrián Gómez <adrianracma21@gmail.com>
2021-10-05 19:25:18 +0200

    Mini css aplicado

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
-<body>
-        <h1><center>Equipo TeamSAJA</center></h1>
-        <ul>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+	<body>
+        	<h1><center>Equipo TeamSAJA</center></h1>
+		<br></br>
+        	<ul>
 
-        <?php 
+        	<?php 
 
-                $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-                foreach( $imgs as $img ) {
-                        if(substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
-                                $name = substr($img,0,-4);
-                                $nameLowerCase = strtolower($name);
-                                if ($nameLowerCase ==  "joseparedes" || $nameLowerCase == "adriangofe" || $nameLowerCase == "samuelmillan" || $nameLowerCase == "alejandrogonzalez") {
-                                        echo "<a href='profile/$name.html'>";
-                                        echo "<center><img src='img/$img' width='130'></center>";
-                                        echo $name."</a>";
-                                }
-                                echo "<div></div>";
-                                ;
-                        }
-                }
-
-        ?>
-
-</body> 
+			echo "<table>";
+			$tabla2 = 0;
+                	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+			foreach( $imgs as $img ) {
+                        	if(substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg") {
+                                	$name = substr($img,0,-4);
+                                	$nameLowerCase = strtolower($name);
+                                	if ($nameLowerCase ==  "joseparedes" || $nameLowerCase == "adriangofe" || $nameLowerCase == "samuelmillan" || $nameLowerCase == "alejandrogonzalez") {
+						echo "<tr>";
+						echo "<td><center><a href='profile/$name.html'>".$name."</center>";
+                                        	echo "<center><img src='img/$img' width='225'></center>";
+						echo "</a></td>";
+						echo "</tr>";
+					}
+                                	echo "<div></div>";
+                                	echo "</table>";
+                        	}
+                	}
+        	?>
+	</body>
+</html>
