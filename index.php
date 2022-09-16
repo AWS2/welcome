0e16fbcb5c230d3d96d245d310666ff47d709484
Enric Mieza <emieza@xtec.cat>
2022-09-16 14:40:23 +0200

    avoid . and .. in the file list

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
 
 	<?php 
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {    
+        foreach( $imgs as $img ) {
+            if( $img=="." || $img==".." )
+                continue;
             if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
                 $name = substr($img,0,-4);
             }else if (substr($img,-4)=="jpeg") {
