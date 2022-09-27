21bfc1a46c28645315254b51c6fdabd90cb83ced
CarlosLopez <karlos.kny@hotmail.com>
2022-09-27 18:30:33 +0200

    añado css y cambios en php

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
+<head>
+<link rel="stylesheet" href="mystyle.css">
+</head>
 <body>
-	<h1>Projecte Welcome 1</h1>
-	<table>
-	<tr>
-	<?php
+    <h1>Projecte Welcome 1</h1>
+    <table>
+    <tr>
+    <?php
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-		$cont = 1;
+        $cont = 1;
         foreach( $imgs as $img ) {
-			if( $cont % 3 == 1) {
-				echo "</tr><tr>";
-			}
+            if( $cont % 3 == 1) {
+                echo "</tr><tr>";
+            }
             if( $img=="." || $img==".." )
                 continue;
             if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
@@ -19,11 +22,10 @@
             echo "<td><a href='profile/$name.html'>";
             echo "<img src='img/$img' width='130'>";
             echo $name."</a></td>";
-			$cont +=1;      
-		}
+            $cont +=1;
+        }
     ?>
-	</tr>
-	</table>
+    </tr>
+    </table>
 
 </body>
-
