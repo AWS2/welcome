36c7c0001a5c190b73ef5ee6e3dd395799a972be
Geriatrico87 <groudon.gvm@gmail.com>
2022-09-27 18:28:15 +0200

    Afegeixo el 2n camvi del fitxer del meu company Jose Duro

--- index.php
+++ index.php
@@ -6,9 +6,27 @@
     <title></title>
     <link href="index.css" rel="stylesheet" type="text/css">
 </head>
+<style>
 
+div {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+  position: relative;
+  animation: myfirst 5s linear 2s infinite alternate;
+}
+
+@keyframes myfirst {
+  0%   {background-color:red; left:0px; top:0px;}
+  25%  {background-color:yellow; left:200px; top:0px;}
+  50%  {background-color:blue; left:200px; top:200px;}
+  75%  {background-color:green; left:0px; top:200px;}
+  100% {background-color:red; left:0px; top:0px;}
+}
+</style>
 <body>
     <h1>Projecte Welcome 1</h1>
+	
     <ul>
 
     <?php 
