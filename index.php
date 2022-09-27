7a52f40a7fd31e4b70028458b5287fba5a8fee7f
Spartanshooter <adriacostasalse@gmail.com>
2022-09-27 18:06:12 +0200

    ultimos retoques

--- index.php
+++ index.php
@@ -5,24 +5,23 @@
 <body>
 	<h1>Projecte Welcome 1</h1>
 	<ul>
-
-	<?php 
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
+        <?php 
+            $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+            foreach( $imgs as $img ) {
+                if( $img=="." || $img==".." )
+                    continue;
+                if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
+                    $name = substr($img,0,-4);
+                }else if (substr($img,-4)=="jpeg") {
+                    $name = substr($img,0,-5);
+                }
+                echo "<div>";
+                echo "<a href='profile/$name.html'>";
+                echo "<img src='img/$img' width='130'>";
+                echo $name."</a>";
+                echo "</div>";
             }
-            echo "<div>";
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "</div>";
-        }
-    ?>
-
+        ?>
+    </ul>
 </body>
 
