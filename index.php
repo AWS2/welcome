3f665974b070f0b970c9e51a597da8fc2d3d7880
Daniel <danielnupa0904@gmail.com>
2022-09-27 17:12:43 +0200

    feat: :sparkles: Añadido código HTML/CSS
    Añadido en el archivo HTML etiquetas y en el CSS class con atributos

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <body>
 	<h1>Projecte Welcome 1</h1>
-	<ul>
+	<ul class="row">
 
 	<?php 
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
@@ -16,12 +16,17 @@
             }else if (substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
             }
+            echo "<div class='column'>";
+            echo "<p>$name.</p>";
+
             echo "<a href='profile/$name.html'>";
             echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
+            echo "</a>";
+            echo "</div>";
         }
     ?>
+
+    </ul>
 
 </body>
 
