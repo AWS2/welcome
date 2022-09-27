19f02bb590681995d7312e053f3f3f6f2191136c
Daniel <danielnupa0904@gmail.com>
2022-09-27 18:13:40 +0200

    feat: :sparkles: Añadidos estilos CSS y un video como fondo

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 </style>
 
 <body>
+<video autoplay muted loop id="myVideo">
+  <source src="https://carontestudio.com/img/f4.mp4" type="video/mp4">
+</video>
 	<h1>Projecte Welcome 1</h1>
 	<ul class="row">
-
-	<?php 
+    	<?php 
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
         foreach( $imgs as $img ) {
             if( $img=="." || $img==".." )
@@ -16,8 +18,9 @@
             }else if (substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
             }
+           
             echo "<div class='column'>";
-            echo "<p>$name.</p>";
+            echo "<p class='glitch'>$name.</p>";
 
             echo "<a href='profile/$name.html'>";
             echo "<img src='img/$img' width='130'>";
