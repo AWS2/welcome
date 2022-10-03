053e607ceeaf7c69fd00a403d35cd93c1858449b
AndresVT24 <andres24vt@gmail.com>
2022-10-03 16:46:35 +0200

    SE VEN LAS IMAGENES

--- index.php
+++ index.php
@@ -1,23 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Webs</title>
+</head>
 <body>
-	<h1>Projecte Welcome 1</h1>
-    <section>
-	<?php 
-            
-        $htmls = scandir("./profile",SCANDIR_SORT_ASCENDING);
-        foreach( $htmls as $html ) {
-            if( $html=="." || $html==".." )
-                continue;
-            if( substr($html,-4)=="html"){
-                $name = substr($html,0,-5);
+    <h1>Projecte Welcome 1</h1>
+        <section>
+        <?php 
+                
+            $htmls = scandir("./profile",SCANDIR_SORT_ASCENDING);
+            foreach( $htmls as $html ) {
+                if( $html=="." || $html==".." )
+                    continue;
+                if( substr($html,-4)=="html"){
+                    $name = substr($html,0,-5);
+                }
+
+                $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+
+                foreach( $imgs as $img ) {
+                    if( $img=="." || $img==".." )
+                        continue;
+                    if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
+                        $name2 = substr($img,0,-4);
+                    }else if (substr($img,-4)=="jpeg") {
+                        $name2 = substr($img,0,-5);
+                    }
+                    if ($name == $name2){
+                        $imgName = $img;
+                    }
+                }
+                
+                echo "<article>\n         ";
+                echo "<a href='profile/$name.html'>";
+                echo "<img src='img/$imgName' width='130'>";
+                echo $name."</a>\n      ";
+                echo "</article>\n          ";
             }
-            echo "<article>\n         ";
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>\n      ";
-            echo "</article>\n          ";
-        }
-    ?>
-    </section>
-
+        ?>
+        </section>
 </body>
-
+</html>
