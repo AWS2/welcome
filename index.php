8223991732f7397b188966c26a64919fd8edab86
raul-garcia-sanchez <raulgarcia200326@gmail.com>
2022-10-03 16:39:12 +0200

    modifying index.php AlexRaul

--- index.php
+++ index.php
@@ -1,23 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
 <body>
 	<h1>Projecte Welcome 1</h1>
 	<ul>
 
 	<?php 
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
+        $profiles = scandir("./profile",SCANDIR_SORT_ASCENDING);
+        $imgExtensions = array("jpg", "jpeg", "png");
+        $image = "";
+        foreach( $profiles as $profile ) {
+            $name = substr($profile,0,-5);
+            $image = "userDefault.png";
+            if( $profile=="." || $profile==".." )
                 continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
+            if(substr($profile,-5) == ".html"){
+                echo "<a href='profile/$name.html'>$name";
+                foreach($imgExtensions as $extension){
+                    if(file_exists("img/".$name.".".$extension)){
+                        $image = $name.".".$extension;
+                    }
+                }
+                echo "<img src='img/$image' alt='Foto de $name' width='130'>";
             }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
+            
+           
         }
     ?>
 
+    </ul>
+
 </body>
 
+</html>
