ca01e65d77b4ec5b34e6763e508713e750c8c521
Unai RM <miperritolukas@gmail.com>
2022-10-03 16:31:49 +0200

    Recorre los html y crea articulos con img y html linkeado

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <body>
 	<h1>Projecte Welcome 1</h1>
-	<ul>
-
+    <section>
 	<?php 
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
+            
+        $htmls = scandir("./profile",SCANDIR_SORT_ASCENDING);
+        foreach( $htmls as $html ) {
+            if( $html=="." || $html==".." )
                 continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
+            if( substr($html,-4)=="html"){
+                $name = substr($html,0,-5);
             }
+            echo "<article>\n         ";
             echo "<a href='profile/$name.html'>";
             echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
+            echo $name."</a>\n      ";
+            echo "</article>\n          ";
         }
     ?>
+    </section>
 
 </body>
 
