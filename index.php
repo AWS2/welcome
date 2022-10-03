2139f8640f32b009b09201ce8ae28ceda8a167d5
PauR03 <priusnori03@gmail.com>
2022-10-03 16:17:18 +0200

    update PauR

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projecte Welcome 1</title>
+</head>
 <body>
-	<h1>Projecte Welcome 1</h1>
+    <h1>Projecte Welcome 1</h1>
 	<ul>
 
 	<?php 
@@ -12,12 +20,11 @@
             }else if (substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
             }
-            echo "<a href='profile/$name.html'>";
+            echo "      <div><a href='profile/$name.html'>";
             echo "<img src='img/$img' width='130'>";
             echo $name."</a>";
-            echo "<div></div>";
+            echo "</div>\n";
         }
     ?>
-
 </body>
-
+</html>
