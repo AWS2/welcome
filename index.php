be296c55ad25e3417ca4140cd60d6893bb7720ee
Alex <alexlarios49@gmail.com>
2022-10-03 17:12:16 +0200

    Improved visual changes

--- index.php
+++ index.php
@@ -5,11 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
+
+    <style>
+        table{
+            width: 70%;
+            margin: 0 auto; 
+            border-spacing: 10px;
+            border: solid black;
+        }
+        td{
+            border: solid black;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
 	<h1>Projecte Welcome 1</h1>
-	<ul>
-
+    <table>
 	<?php 
         $profiles = scandir("./profile",SCANDIR_SORT_ASCENDING);
         $imgExtensions = array("jpg", "jpeg", "png");
@@ -20,21 +32,19 @@
             if( $profile=="." || $profile==".." )
                 continue;
             if(substr($profile,-5) == ".html"){
-                echo "<a href='profile/$name.html'>$name";
+                echo "<tr>\n";
                 foreach($imgExtensions as $extension){
                     if(file_exists("img/".$name.".".$extension)){
                         $image = $name.".".$extension;
                     }
                 }
-                echo "<img src='img/$image' alt='Foto de $name' width='130'>";
-            }
-            
-           
+                echo "<td><img src='img/$image' alt='Foto de $name' width='130'></td>\n";
+                echo "<td><a href='profile/$name.html'>$name</td>\n";
+                echo "</tr>\n";
+            }           
         }
     ?>
-
-    </ul>
-
+    </table>
 </body>
 
 </html>
