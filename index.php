9a0719a452767edf15b6035ca71bcafff6e59719
PauR03 <priusnori03@gmail.com>
2022-10-03 16:20:22 +0200

    index.php

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projecte Welcome 1</title>
-</head>
 <body>
-    <h1>Projecte Welcome 1</h1>
+	<h1>Projecte Welcome 1</h1>
 	<ul>
 
 	<?php 
@@ -20,11 +12,11 @@
             }else if (substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
             }
-            echo "      <div><a href='profile/$name.html'>";
+            echo "<a href='profile/$name.html'>";
             echo "<img src='img/$img' width='130'>";
             echo $name."</a>";
-            echo "</div>\n";
+            echo "<div></div>";
         }
     ?>
+
 </body>
-</html>
