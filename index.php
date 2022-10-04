8dc5c5816c3090620b956fb7d9d1474c1bfdd0ec
Pau Rius <priusnori03@gmail.com>
2022-10-04 11:15:46 +0200

    pr

--- index.php
+++ index.php
@@ -34,20 +34,6 @@
 
             }
         }
-        // $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        // foreach( $imgs as $img ) {
-        //     if( $img=="." || $img==".." )
-        //         continue;
-        //     if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-        //         $name = substr($img,0,-4);
-        //     }else if (substr($img,-4)=="jpeg") {
-        //         $name = substr($img,0,-5);
-        //     }
-        // echo "<div><a href='profile/$html.html'>";
-        // echo "<img src='img/$imagen' width='130'>";
-        // echo $name."</a>";
-        // echo "</div>\n";
-        //}
     ?>
 </body>
 </html>
