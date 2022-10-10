87bd472cc1cf65f3f8ccbe9344f26c376ce192d7
Xavi Martinez <77339181+xaavii0711@users.noreply.github.com>
2022-10-10 16:28:06 +0200

    Add index.php

--- index.php
+++ index.php
@@ -54,8 +54,6 @@
         $imgFormat = ["jpg", "jpeg", "png"];
         echo "<tr>";
 
-        
-
         foreach( $profiles as $prof ) {
             if($numero %3 == 0){
                 echo "</tr>";
@@ -65,12 +63,18 @@
                 continue;
             }if (substr($prof,-4)=="html") {
                 $name = substr($prof,0,-5);
+                foreach($imgFormat as $format){
+                    if(file_exists("img/".$name.".".$format)){
+                        $imgProfile = $name.".".$format;
+                    }
+                }
+                echo "<td>";
+                echo "\n<br><a href='profile/$prof'>";
+                echo "<img src='img/$imgProfile' width='130'></a>";
+                echo "\n<br><a href='profile/$prof'>".$name."</a>";
+                echo "</td>";
             }
-            echo "<td>";
-            echo "\n<br><a href='profile/$prof'>";
-            echo "<img src='img/$name.jpg' width='130'></a>";
-            echo "\n<br><a href='profile/$prof'>".$name."</a>";
-            echo "</td>";
+            
         }
 
 
