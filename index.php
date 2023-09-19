4656b2887ba41c06abacefae2f0b7e0d1e48707e
Yuheng <yzhou.cf@iesesteveterradas.cat>
2023-09-19 16:14:14 +0200

    arreglar

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
-<body>
-	<h1>Projecte Welcome 1</h1>
-	<ul>
+<h1>El fabuloso mundo de Jordi i Genis</h1>
 
-	<?php 
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-        }
-    ?>
+<ul>
 
-</body>
+<?php
+	$imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+	foreach( $imgs as $img ) {
+		
+		if( substr($img,(-3 || -4))==("jpg" || "png" || "jpeg")) {
+			$name = substr($img,0,-4);
+			echo "<a href='profile/$name.html'>";
+			echo "<img src='img/$img' width='130'><br/>";
+			echo $name."</a>";
+			echo "<br /><br />";
+		}
+
+
+	}
+	
+?>
+
+</ul>
