cd254bae6180b12c86b2639987e16fc7d8b1e1a7
PNaharro <Pauhtto@gmail.com>
2023-09-19 16:19:53 +0200

    img y profile

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <body>
 	<h1>Projecte Welcome 1</h1>
+    <style>
+        a{
+            padding-top: 50px;
+            padding-right: 30px;
+            padding-bottom: 50px;
+            padding-left: 80px;
+        }
+        img{
+            border-radius: 50%;
+        }
+    </style>
 	<ul>
-
 	<?php 
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
         foreach( $imgs as $img ) {
@@ -16,7 +26,8 @@
             echo "<img src='img/$img' width='130'>";
             echo $name."</a>";
             echo "<div></div>";
+            
         }
     ?>
-
+    </il>
 </body>
