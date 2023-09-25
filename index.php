b7765d7d1eeedd2c9070f7df473e846403c72270
MatillaSan <dsanchezmatilla.cf@iesesteveterradas.cat>
2023-09-25 17:07:10 +0200

    Modificado PHP y añadido CSS para los div's

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <head>
     <link rel="stylesheet" href="estilopagina.css">
+    <title>Meet the future Professionals</title>
 </head>
 <body>
 	<h1>Projecte Welcome 1</h1>
-	<ul>
-
+	
+	<div class = "main">
 	<?php 
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
         foreach( $imgs as $img ) {
+            $divClass = "plebe";
             if( $img=="." || $img==".." )
                 continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
+
+            if ($img == "AA0DavidSanchezMatilla.jpg"){
+                $divClass = "masterChief";
+                $name = "AA0DavidSanchezMatilla";
+
+            }else if($img == "GeanfrancoBiagi.jpeg"){
+                $divClass = "masterChief";
+                $name = "GeanfrancoBiagi";
+
+            }else if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
                 $name = substr($img,0,-4);
+
             }else if (substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
-            }
+            } 
+            echo "<div class = $divClass>";
             echo "<a href='profile/$name.html'>";
             echo "<img src='img/$img' width='130'>";
             echo $name."</a>";
-            echo "<div></div>";
+            echo "</div>";
+            echo "\n";
         }
     ?>
+    </div>
 </body>
