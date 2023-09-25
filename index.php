528a7d1c0cd57e32aa17640931f7a420ed89c418
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2023-09-25 10:14:10 +0200

    Estructura

--- index.php
+++ index.php
@@ -3,25 +3,32 @@
 
 </head>
 <body>
-    
-	<h1>Projecte Welcome 1</h1>
+  
+<div class="header">
+    <h1>Projecte Welcome 1</h1>
+</div>
+
 	<ul>
+        <table>
 
 	<?php 
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
         foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
+            
             if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-        }
-    ?>
+                $name = substr($img,0,-4);?>
+                <tr>
+                <td>
+                    <?php echo "<a href='profile/$name.html'><img  src='img/$img' ></a><br/>";
+                            echo "<h3>";
+                            echo $name."</h3>";?>
+                            </td>
+                        </tr>
+                   <?php }?>
+                <?php
 
+
+            }
+        ?>
+        </table>
 </body>
