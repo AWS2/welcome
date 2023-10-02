928750cb5e5d25d15ca395c128b28b3412e8879d
IFurio <ifuriomartin.cf@iesesteveterradas.cat>
2023-10-02 17:04:41 +0200

    in progress php

--- index.php
+++ index.php
@@ -1,6 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projecte Welcome 1</title>
+    <style>
+        body {
+            font-family: "Open Sans", sans-serif;
+            user-select: none;
+            background-color: #eef0f4;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        ul {
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+            list-style-type: none;
+        }
+        ul li {
+        display:inline-block;
+        }
+        img {
+            height: 100%;
+            width: 100%;
+            z-index: -1;
+            position: sticky;
+            top: 1px;
+        }
+        a {
+            z-index: 10;
+        }
+    </style>
+</head>
 <body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
+    <h1>Projecte Welcome 1</h1>
+    <ul>
         <?php
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
         foreach( $imgs as $img ) {
@@ -11,12 +46,10 @@
             }else if (substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
             }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
+            echo "<li><a href='profile/$name.html'><img src='img/$img'></a><a href='profile/$name.html'>$name</a></li>";
 
         }
-    ?>
-    </il>
+        ?>
+    </ul>
 </body>
+</html>
