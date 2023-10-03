c7a2640289d1b3150ab1621bb8e173e4f6165940
Micronoft <dam2002@gmail.com>
2023-10-03 16:12:57 +0000

    wkjfnwejikfnwekfnweklfnweokfg3krjwngowejngkwejlnfjn

--- index.php
+++ index.php
@@ -1,22 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
 <body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-
-        }
+  <h1>Projecte Welcome 1</h1>
+  <ul>
+    <?php
+    $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
+    foreach ($imgs as $img) {
+      if ($img == "." || $img == "..") {
+        continue;
+      }
+      if (substr($img, -3) == "jpg" || substr($img, -3) == "png") {
+        $name = substr($img, 0, -4);
+      } else if (substr($img, -4) == "jpeg") {
+        $name = substr($img, 0, -5);
+      }
+      echo "<li><a href='profile/$name.html'>";
+      echo "<img src='img/$img' width='130'>";
+      echo "<div>" . $name . "</div></a></li>";
+    }
     ?>
-    </il>
+  </ul>
 </body>
+</html>
