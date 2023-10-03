c38248a82c86c3999dc741b7a629e1efcf0eb8a9
sara <santamariasara123@gmail.com>
2023-10-03 19:06:01 +0200

    Cambios realizados

--- index.php
+++ index.php
@@ -3,11 +3,15 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <h1 class="title">La MASA team</h1>
-    <table class="table">
-        <tr>
-            <th>Imagen</th>
-            <th>User</th>
+    <div class="card">
+        <div class="card-body">
+            <h1 class="title">La MASA team</h1>
+        </div>
+    </div>
+    <table class="table table-primary">
+        <tr class="table-light">
+            <th class="textaligne">Imagen</th>
+            <th class="textaligne">User</th>
         </tr>
         <?php 
         $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
