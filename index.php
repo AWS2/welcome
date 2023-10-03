66d6eb7a43c984f0ae846e76dc183151f143a35f
Alexx098 <garciavalencianoalex@gmail.com>
2023-10-03 18:43:29 +0200

    Cambio Estilo commit n1 Alex Garcia

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <head>
 <link rel="stylesheet" type="text/css" href="styles.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <h1 class="title">Projecte Welcome 1</h1>
-    <table class="imageTable">
+    <h1 class="title">La MASA team</h1>
+    <table class="table">
         <tr>
             <th>Imagen</th>
-            <th>Nombre</th>
+            <th>User</th>
         </tr>
         <?php 
         $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
