75e3889dee10397eb4def12f54426a7883d03aad
MarcG02 <marcgalaxy02@gmail.com>
2023-10-03 18:30:28 +0200

    Implementacion de css para generar tabla

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
+<head>
+<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
 <body>
-	<h1>Projecte Welcome 1</h1>
-	<ul>
-	<?php 
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
+    <h1 class="title">Projecte Welcome 1</h1>
+    <table class="imageTable">
+        <tr>
+            <th>Imagen</th>
+            <th>Nombre</th>
+        </tr>
+        <?php 
+        $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
+        foreach($imgs as $img) {
+            if($img == "." || $img == "..") {
                 continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
             }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-            
+            if(substr($img, -3) == "jpg" || substr($img, -3) == "png") {
+                $name = substr($img, 0, -4);
+            } elseif(substr($img, -4) == "jpeg") {
+                $name = substr($img, 0, -5);
+            }
+            echo "<tr>";
+            echo "<td><a class='tableImage' href='profile/$name.html'><img src='img/$img' width='130'></a></td>";
+            echo "<td class='tableName'><a href='profile/$name.html'>$name</a></td>";
+            echo "</tr>";
         }
-    ?>
-    </il>
+        ?>
+    </table>
 </body>
