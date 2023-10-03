183b5a73a1197eff23910c8d7e497874d0a5d0a3
AndreuAB <aabellansole.cf@iesesteveterradas.cat>
2023-10-03 18:26:03 +0200

    cositas

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
 <body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
+    <div align='center'><h1 id="title">Projecte Welcome 1</h1></div>
+    <div class="grid-container">
+        <?php 
+            $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
+            $imageCount = 0;
 
-        }
-    ?>
-    </il>
+            foreach ($imgs as $img) {    
+                if (substr($img, -3) == "jpg" || substr($img, -3) == "png" || substr($img, -4) == "jpeg") {
+                    $name = substr($img, 0, -4);
+                    echo "<div class='grid-item'>";
+                    echo "<a href='profile/$name.html'><br>";
+                    echo "<img src='img/$img' width='150' height='150'><br>";
+                    echo $name . "</a><br></div>";
+                    $imageCount++;
+
+                    if ($imageCount % 4 == 0) {
+                        echo "<div class='grid-row'></div>";
+                    }
+                }
+            }
+        ?>
+    </div>
 </body>
+</html>
+
+
