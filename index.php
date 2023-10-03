ff1b9d5e648dc45c5116d14b63a617b0b29db2bb
adrianmarin <adrian.marindiaz10@gmail.com>
2023-10-03 19:10:12 +0200

    Nuevo cambio en la branca

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 <body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
+    <h1>Projecte Welcome 1</h1>
+    <table>
+        <tr>
+            <th>Columna 1</th>
+            <th>Columna 2</th>
+        </tr>
         <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
+        $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
+        foreach ($imgs as $img) {
+            if ($img == "." || $img == "..") continue;
+            if (substr($img, -3) == "jpg" || substr($img, -3) == "png") {
+                $name = substr($img, 0, -4);
+            } else if (substr($img, -4) == "jpeg") {
+                $name = substr($img, 0, -5);
             }
-            echo "<a href='profile/$name.html'>";
+            echo "<tr>";
+            echo "<td><a href='profile/$name.html'>";
             echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-
+            echo "</a></td>";
+            echo "<td>$name</td>";
+            echo "<td></td>";
+            echo "<td></td>";
+            echo "</tr>";
         }
-    ?>
-    </il>
+        ?>
+    </table>
 </body>
+</html>
