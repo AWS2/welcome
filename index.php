98e0a313602ffd71fcb760f074982ae22a776f6b
Tianle <tianleyin8888@gmail.com>
2023-10-09 15:33:33 +0200

    Change index

--- index.php
+++ index.php
@@ -1,22 +1,74 @@
-<body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
+<!DOCTYPE html>
+<html>
 
+<head>
+    <title>Index</title>
+    <style>
+        body {
+            background-color: black;
+            width: 1200px;
+            color: whitesmoke;
+            margin: auto;
+            justify-items: center;
         }
-    ?>
-    </il>
+        h1 {
+            text-align: center;
+        }
+        .cuad {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            justify-items: center;
+            float: left;
+            margin: 10px;
+            padding: 15px;
+            padding-left: 30px;
+            padding-right: 30px;
+            background-color: #1a1926;
+        }
+
+        a {
+            text-decoration: none;
+            color: whitesmoke;
+            width: 150px;
+            margin: 0;
+            padding: 0;
+        }
+        img {
+            max-width: 100%;
+            height: 130px;
+            margin: auto;
+            align-content: center;
+            align-self: center;
+        }
+        p {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Projecte Welcome 1</h1>
+    <ul>
+            <?php
+            $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
+            foreach ($imgs as $img) {
+                if ($img == "." || $img == "..")
+                    continue;
+                if (substr($img, -3) == "jpg" or substr($img, -3) == "png") {
+                    $name = substr($img, 0, -4);
+                } else if (substr($img, -4) == "jpeg") {
+                    $name = substr($img, 0, -5);
+                }
+                echo "<div class='cuad'>";
+                echo "<a href='profile/$name.html'>";
+                echo "<img src='img/$img'>";
+                echo "<p>$name</p>" . "</a>";
+                echo "</div>";
+            }
+            ?>
+
+    </ul>
 </body>
+
+</html>
