b088fbf5780829432e8612167e371c52bba86407
MehdiRegg777 <96047417+MehdiRegg777@users.noreply.github.com>
2023-10-09 16:00:43 +0200

    Mehdi

--- index.php
+++ index.php
@@ -1,22 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="tablas.css" rel="stylesheet" type="text/css">
+    <title>Projecte Welcome 1</title>
+</head>
 <body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
+	<h1>Projecte Welcome 1</h1>
+	<?php 
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+        $i++;
+        echo "<table >";
         foreach( $imgs as $img ) {
+            if($i % 3 == 0){
+                echo "<tr class='celdas'></tr>";   
+            }
+            $i++;
             if( $img=="." || $img==".." )
                 continue;
             if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
                 $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
+            }else if(substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
             }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-
+            echo "<td class='celdas'>\n";
+            echo "<a href='profile/$name.html'><br>\n";
+            echo "<img src='img/$img' width='130'></a><br>\n";
+            echo "<a href='profile/$name.html'>".$name."</a>\n";
+            echo "</td>\n";
         }
+        echo "</table>";
     ?>
-    </il>
 </body>
+</html>
