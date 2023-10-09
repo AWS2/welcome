a562e66263e782c3d64efbcdaa602fd3a30137a8
DiegoParesL <diegoparesl@hotmail.com>
2023-10-09 15:56:04 +0200

    branch Diego Pares

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome!</title>
     <style>
-        
+        li{
+            list-style-type:none;
+        }
+        h1{
+            text-align: center;
+            background-color:rgba(0,0,0,0.15);
+            border-radius:20%;
+            width:fit-content;
+            margin: 0 auto;
+            padding-left:20px;
+            padding-right:20px;
+        }
+        body{
+            background-color: azure;
+        }
         ul{
             display:flex;
             flex-direction:row;
@@ -18,24 +32,24 @@
         }
         div a{
             width: fit-content;
-            box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
             
         }
         div .container {
+            margin-top: 30px;
             display:flex;
-            flex-direction:column;
             width: calc((80vw/3) - 30px);
-            height: calc(80vw/3 - 30px);
-            background-color: azure;
+            height: 250px;
+            background-color: rgba(0,255,255,0.3);
             box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
             vertical-align:bottom;
             justify-content: space-around;
-
+            border-radius:5%;
         }
         img {
-            max-height:calc((80vh/3) - 30px);
+            height:calc((80vh/3) - 30px);
             width :calc((80vw/3) - 30px);
             vertical-align: bottom;
+            border-radius:5%;
         }
     </style>
 </head>
@@ -54,12 +68,13 @@
             }else if (substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
             }
+            echo "<li>";
             echo "<div class='container'>";
             echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img'>";
+            echo "<img src='img/$img' alt='img alumnes'>";
             echo $name."</a>";
             echo "</div>";
-
+            echo "</li>";
         }
     ?>
     </ul>
