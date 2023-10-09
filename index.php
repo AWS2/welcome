7ac2d3bc41dc973e648393aec29dc2440ac6f26c
DiegoParesL <diegoparesl@hotmail.com>
2023-10-09 16:11:34 +0200

    branch Diego Pares

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Diego Pares" >
     <title>Welcome!</title>
     <style>
         li{
