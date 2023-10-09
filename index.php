949af9b5dd51f09aef1b54308166ebb1f93c0737
pmaestre03 <maestrep15@gmail.com>
2023-10-09 16:01:05 +0200

    estilos PMaestre

--- index.php
+++ index.php
@@ -1,22 +1,55 @@
-<body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Pau Maestre</title>
+    <style>
+        body {
+            background-color: grey;
+            margin-left: 10%;
+            margin-right: 10%;
+            
         }
-    ?>
-    </il>
+        h1 {
+            text-align: center;
+        }
+        ul {
+            columns: 4;
+        }
+        li {
+            margin-bottom: 10px;
+            display: flex;
+            align-items: center;
+            list-style-type: none;
+            align-items: center;
+            width: 130px;
+        }
+        a {
+            overflow: hidden;
+        }
+    </style>
+</head>
+<body>
+<h1>Projecte Welcome 1</h1>
+<ul>
+        <?php
+        $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
+        foreach ($imgs as $img) {
+            if ($img == "." || $img == "..")
+                continue;
+            if (substr($img, -3) == "jpg" or substr($img, -3) == "png") {
+                $name = substr($img, 0, -4);
+            } else if (substr($img, -4) == "jpeg") {
+                $name = substr($img, 0, -5);
+            }
+            echo "<li>";
+            echo "<a href='profile/$name.html'>";
+            echo "<img src='img/$img' width='130' height='130'>";
+            echo "<br>".$name ."</a>\n";
+            echo "<li>";
+        }
+        ?>
+    </ul>
 </body>
+</html>
