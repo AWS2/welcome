4e088e9594a4adb2cfd91a721c20bfd697e0f4bc
lina2210 <lina23@pluralcamp.com>
2023-10-09 16:25:02 +0200

    css

--- index.php
+++ index.php
@@ -1,6 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projecte Welcome</title>
+    <style>
+
+        body {
+            background-color: #d3d3d3;
+        }
+
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+       
+        }
+
+        ul {
+            list-style: none;
+            
+        }
+
+        a {
+            text-align: center;
+        }
+
+        .profile {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 400px;
+            height: auto;
+            max-width: 400px;
+            border: 2px solid black;
+            margin: 20px;
+            
+        }
+
+        img {
+            width: 200px;
+            height: 200px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        
+
+    
+
+        
+        
+    </style>
+</head>
 <body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
+    <h1>Projecte Welcome 1</h1>
+    <div class="container">
         <?php
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
         foreach( $imgs as $img ) {
@@ -11,12 +69,20 @@
             }else if (substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
             }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-
+                echo "<div class='profile'>";
+                echo "<ul>";
+                    echo "<li><a href='profile/$name.html' id='$name'>";
+                    echo "<li><img src='img/$img' alt='$name' width='130'></li>";
+                    echo $name."</a></li>";
+                echo "</ul>";
+                echo "</div>";
+            
         }
-    ?>
-    </il>
+        ?>  
+    </div>
+
 </body>
+</html>
+
+
+        
