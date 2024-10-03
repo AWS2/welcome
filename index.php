5473f7930c743b59f45644fbf48f5da736fbc63d
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2024-10-03 23:24:20 +0200

    Se ha cambiado el index.php para mejorar el codigo y el index.css para aplicar cambios visuales

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Projecte Welcome 1</title>
+    <link rel="stylesheet" href="index.css">
+</head>
 <body>
         <h1>Projecte Welcome 1</h1>
         <ul>
         <?php
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
+        foreach($imgs as $img) {
+            if ($img=="." || $img=="..")
                 continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
+            if (substr($img,-3)=="jpg" || substr($img,-3)=="png"){
                 $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
+            } elseif (substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
             }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-
+            
+            if (isset($name)){
+                echo "<a href='profile/$name.html'>";
+                echo "<img src='img/$img' width='130'>";
+                echo "<p>".$name."</p></a>";
+            }
         }
     ?>
-    </il>
+    </ul>
 </body>
+</html>
