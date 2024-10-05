cfe73c8301c939f797e29af567c5738687759b60
estillicon <paugracia86@yahoo.com>
2024-10-05 10:54:13 +0200

    actualizacion index.php,css

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
 <body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
+    <h1>Projecte Welcome 1</h1>
+    <div class="container">
         <?php
+        
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
         foreach( $imgs as $img ) {
             if( $img=="." || $img==".." )
@@ -11,12 +22,22 @@
             }else if (substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
             }
+            echo "<div class='item'>";
             echo "<a href='profile/$name.html'>";
             echo "<img src='img/$img' width='130'>";
+            echo "<br>";
             echo $name."</a>";
-            echo "<div></div>";
+            echo "</div>";
+            //echo "<div></div>";
 
         }
     ?>
-    </il>
+    </div>
+    
 </body>
+    
+
+</html>
+
+
+
