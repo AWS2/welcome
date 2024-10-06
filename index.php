c60499e1668b926c5ba97ac3b6be1b40cb521018
Erikpr04 <erikpr32@gmail.com>
2024-10-07 00:17:19 +0200

    Added styles and changes on web's visualization

--- index.php
+++ index.php
@@ -1,22 +1,36 @@
-<body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alumnes AWS2</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+    
 
+<div>
+        <h1>Projecte Welcome 1</h1>
+        <div class="main-container">
+        <?php
+        $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
+        foreach ($imgs as $img) {
+            if ($img == "." || $img == "..")
+                continue;
+            if (substr($img, -3) == "jpg" || substr($img, -3) == "png") {
+                $name = substr($img, 0, -4);
+            } else if (substr($img, -4) == "jpeg") {
+                $name = substr($img, 0, -5);
+            }
+            
+            echo "<div class='item-container'>";
+            echo "<a href='profile/$name.html'>";
+            echo "<img src='img/$img' alt='$name'>";
+            echo "<h3>$name</h3>";
+            echo "</a>";
+            echo "</div>";
         }
-    ?>
-    </il>
+        ?>
+        </div>
 </body>
+
+</html>
