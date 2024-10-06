39e3313e60468fd46a519a95512ba80504ac9aa8
Mar <mar.melich@gmail.com>
2024-10-06 23:17:46 +0200

    cambios php y css

--- index.php
+++ index.php
@@ -1,22 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="index.css">
+</head>
 <body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
+    <div>
+    <h1>˚ʚ ❀ ɞ˚ Projecte Welcome 1 - Mar Mèlich ˚ʚ ❀ ɞ˚</h1>
 
+    <table>
+    <?php
+    $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+    $contador = 0;
+    echo "<tr>\n";
+    foreach( $imgs as $img ) {
+        if( $img=="." || $img==".." ){
+            continue;
         }
+        if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
+            $name = substr($img,0,-4);
+        }else if (substr($img,-4)=="jpeg") {
+            $name = substr($img,0,-5);
+        }
+        echo "<td>\n";
+        echo "<a href='profile/$name.html'>";
+        echo "<img src='img/$img' width='130'></a>\n<a>";
+        echo "<a href='profile/$name.html'>";
+        echo $name."</a>\n";
+        echo "</td>\n\n";
+        $contador +=1;
+        if ( $contador ==3){
+            echo "</tr>\n\n <tr>\n";
+            $contador = 0;
+        }
+
+    }
     ?>
-    </il>
+    </table>
+    </div>
+    
 </body>
+</html>
