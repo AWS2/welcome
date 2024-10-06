86d411fc87d60290742fd394ed753a595088ecd7
Victor <vvalerolopez.eb@iesesteveterradas.cat>
2024-10-06 23:25:19 +0200

    PHP arreglado y css hecho

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projecte Welcome 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
+    <h1>Projecte Welcome 1</h1>
+    <div class="card-container">
+    <?php
+        $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
+        foreach ($imgs as $img) {
+            if ($img == "." || $img == "..") {
                 continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
+            }
+            if (substr($img, -3) == "jpg" || substr($img, -3) == "png") {
+                $name = substr($img, 0, -4);
+            } else if (substr($img, -4) == "jpeg") {
+                $name = substr($img, 0, -5);
             }
             echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-
+            echo "<div class='card'>";
+            echo "<img src='img/$img' alt='$name' class='card-image'>";
+            echo "<div class='card-name'>$name</div>";
+            echo "</div>";
+            echo "</a>";
         }
     ?>
-    </il>
+    </div>
 </body>
+</html>
