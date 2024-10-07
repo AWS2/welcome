b5dfd821965dc99d361eeb251438b76e30dfe17d
josehtat <jhurtadot33@gmail.com>
2024-10-07 18:16:47 +0200

    terminado

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
-<body>
+<!Doctype html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tabla de Imágenes</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
         <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
 
-        }
-    ?>
-    </il>
-</body>
+        <table>
+            <?php
+                $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
+                foreach( $imgs as $img ) {
+                    if( $img == "." || $img == ".." ) continue;
+
+                    if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
+                        $name = substr($img, 0, -4);
+                    } elseif (substr($img,-4)=="jpeg") {
+                        $name = substr($img, 0, -5);
+                    }
+
+                    echo "<td>";
+                    echo "<div>";
+                    echo "<a href='profile/$name.html'>$name</a>";
+                    echo "<a href='profile/$name.html'><img src='img/$img' class='float'></a>";
+                    echo "</div>";
+                    echo "</td>";
+                }
+            ?>
+        </table>
+    </body>
+</html>
