999c0fe02c249ad81cedee6c10985df0dc05a96b
sergiofdce <150951059+sergiofdce@users.noreply.github.com>
2024-10-07 19:51:16 +0200

    Mejorar estructura de la tabla de contenido y descripción de imagen en index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +10,6 @@
         <h1>Welcome - sergiofdce</h1>
 
         <div id=TablaCSS>
-        <ul>
             <?php
             $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
 
@@ -46,8 +45,7 @@
             echo "</table>";
             ?>
 
-        </ul>
-        </div>
+</div>
     
 </body>
 </html>
