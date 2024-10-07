ebe3639dfb5760cc655d79f5655385bf788ace7b
jpachonguerra <javier.pachon.guerra@gmail.com>
2024-10-07 19:01:26 +0200

    afegir branch

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    
+</body>
+</html>
 <body>
         <h1>Projecte Welcome 1</h1>
         <ul>
         <?php
         $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+        echo '<table>';
         foreach( $imgs as $img ) {
             if( $img=="." || $img==".." )
                 continue;
@@ -11,12 +24,11 @@
             }else if (substr($img,-4)=="jpeg") {
                 $name = substr($img,0,-5);
             }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
-
+            echo "<tr><td><a href='profile/$name.html'>";
+            echo "<img src='img/$img' alt='foto de perfil' width='130'></a></td>";
+            echo "<td><a href='profile/$name.html'>".$name."</a></td></tr>";
         }
+        echo '</table>';
     ?>
-    </il>
+    </ul>
 </body>
