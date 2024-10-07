352ae01f5b780a624e7ca67d6135329ce64ac097
sergiofdce <150951059+sergiofdce@users.noreply.github.com>
2024-10-07 18:13:36 +0200

    Mejorar descripción de imagen en la tabla de contenido

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 
                 echo "<tr>";
                 // Columna 1: Imagen
-                echo "<td><img src='img/$img' width='130' alt='imagen'></td>";
+                echo "<td><img src='img/$img' width='130' alt='Imagen de Perfil'></td>";
                 // Columna 2: Enlace
                 echo "<td><a href='profile/$name.html'>$name</a></td>";
                 
