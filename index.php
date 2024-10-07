55f49387a8f07f447a82c4f6824372ceaae9977b
adriaht <adriah.t.22@gmail.com>
2024-10-07 18:15:47 +0200

    rama creada y actualizada

--- index.php
+++ index.php
@@ -1,22 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>indice Adria Hernandez</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='index.css'>
+</head>
 <body>
         <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
+        
+            
+                <?php
+                $contador=0;
+                echo "<table>";
+                    $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
+                    foreach( $imgs as $img ) {
+               
+                        if( $img=="." || $img==".." )
+                            continue;
+                        if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
+                            $name = substr($img,0,-4);
+                        }else if (substr($img,-4)=="jpeg") {
+                            $name = substr($img,0,-5);
+                        }
 
-        }
-    ?>
-    </il>
+
+                       
+                       
+
+                      if ($contador==0){
+                        echo "<tr>";
+
+                      }
+                      if ($contador==2){
+                        echo "</tr>";
+                        $contador=0;
+
+                        }
+                        
+                            echo "<td>";    
+                                echo "<a href='profile/$name.html'>";
+                                echo "<img src='img/$img' width='130' alt='imagen'>";
+                            echo "</td>";   
+                            echo "<td>"; 
+                                echo $name."</a>";
+                                echo "<div></div>";
+                                echo "</td>";
+
+                            $contador++;
+                            
+                        
+
+                    }
+                    echo "</table>";
+                ?>
+            
+        </table>
 </body>
+</html>
