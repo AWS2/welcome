347780bb6d2ec4ef53ea71a3d4051b94b58a607d
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2025-10-06 18:11:03 +0200

    altres canvis al indez

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -66,64 +65,3 @@
     </div>
 </body>
 </html>
-=======
-<body>
-    <style>
-        .grid {
-            display: grid;
-            grid-template-columns: repeat(4, 1fr); /* 4 Columnas */
-            gap: 20px; /* Espacio entre las tarjetas */
-            padding: 20px;
-        }
-        .card {
-            background: lightgrey;
-            border-radius: 10px;
-            box-shadow: 0 2px 5px rgba(0,0,0,0.1);
-            padding: 10px;
-            text-align: center;
-            transition: transform 0.2s ease;
-
-        }
-        .card:hover {
-            transform: scale(1.05);
-        }
-        .card img {
-            width: 80%;
-            height: 300px;
-            object-fit: cover; /* Para que se vean proporcionalmente */
-            border-radius: 8px;
-        }
-        h1 {
-            text-align: center;
-            color: darkblue;
-            margin: 5px, 5px, 5px, 5px;
-            font-size: 50px;
-        }
-    </style>
-    <h1>Projecte Welcome  1</h1>
-    <div class="grid">
-    <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            } else {
-                continue;
-            }
-
-            // Tarjeta de imagen y nombres
-            echo "<div class='card'>";
-                echo "<a href='profile/$name.html'>";
-                echo "<img src='img/$img' width='130'>";
-                echo "<h3>$name</h3";
-                echo "</a>";
-            echo "</div>";
-        }
-    ?>
-    </div>
-</body>
->>>>>>> ff5c2c3344d7117bc0b43bae8533671e02d62927
