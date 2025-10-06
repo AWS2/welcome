8f45fe1d5e66117f245d54475c6ea2a1414fb8f7
Mangell06 <mangell0624@gmail.com>
2025-10-06 17:37:52 +0200

    Php

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <body>
-        <h1>Projecte Welcome 1</h1>
-        <ul>
+    <h1>Projecte Welcome 1</h1>
+    <ul>
         <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
+        $imgs = scandir("./img", SCANDIR_SORT_ASCENDING);
+        foreach ($imgs as $img) {
+            if ($img == "." || $img == "..")
                 continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
+            $ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
 
+            if (in_array($ext, ["jpg", "png", "jpeg"])) {
+                $name = pathinfo($img, PATHINFO_FILENAME);
+                echo "<li>";
+                echo "<a href='profile/$name.html'>";
+                echo "<img src='img/$img' width='130' alt='$name'>";
+                echo " $name";
+                echo "</a>";
+                echo "</li>";
+            }
         }
-    ?>
-    </il>
+        ?>
+    </ul>
 </body>
